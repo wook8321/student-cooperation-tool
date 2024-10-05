5daf01d8da043e2bc6d0aa82eea0c78bd23a7b29
laipuni <2tgb02023@gmail.com>
2024-10-05 12:03:54 +0900

    [BackEnd] feat : Member 엔티티 서비스 계층 MemberService 생성

---
+++ src/main/java/com/stool/studentcooperationtools/domain/member/service/MemberService.java
@@ -0,0 +1,11 @@
+package com.stool.studentcooperationtools.domain.member.service;
+
+import com.stool.studentcooperationtools.domain.member.controller.response.MemberSearchResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MemberService {
+    public MemberSearchResponse findFriends() {
+        return null;
+    }
+}
