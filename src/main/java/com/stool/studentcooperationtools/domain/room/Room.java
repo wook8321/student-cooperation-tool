9916397c7c4c9432fa361a2c6f6c0fa8c76ca93c
laipuni <2tgb02023@gmail.com>
2024-10-05 16:29:42 +0900

    [BackEnd] feat : Room 엔티티 생성
    현재 추가한 연관 관계는 Topic만 추가 했고, 추후에 추가될 예정

---
+++ src/main/java/com/stool/studentcooperationtools/domain/room/Room.java
@@ -0,0 +1,32 @@
+package com.stool.studentcooperationtools.domain.room;
+
+import com.stool.studentcooperationtools.domain.BaseTimeEntity;
+import com.stool.studentcooperationtools.domain.Topic.Topic;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Room extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true,length = 20)
+    private String title;
+
+    @Column
+    private int participationNum;
+
+    @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    private Topic topic;
+
+    public String getTopic(){
+        return this.topic.getTopic();
+    }
+
+}
