35e46036f60fa9c1bf167479581c7a1645aaf204
laipuni <2tgb02023@gmail.com>
2024-10-05 13:10:48 +0900

    [BackEnd] test : 친구 상태 아닌 유저와 친구 상태 유저 검색 및 조회 테스트 및 asciidocs 추가

--- src/test/java/com/stool/studentcooperationtools/docs/member/MemberApiControllerDocsTest.java
+++ src/test/java/com/stool/studentcooperationtools/docs/member/MemberApiControllerDocsTest.java
@@ -2,8 +2,10 @@ package com.stool.studentcooperationtools.docs.member;
 
 import com.stool.studentcooperationtools.docs.RestDocsSupport;
 import com.stool.studentcooperationtools.domain.member.controller.MemberApiController;
+import com.stool.studentcooperationtools.domain.member.controller.request.MemberSearchMemberDto;
 import com.stool.studentcooperationtools.domain.member.controller.response.MemberFindMemberDto;
 import com.stool.studentcooperationtools.domain.member.controller.response.MemberFindResponse;
+import com.stool.studentcooperationtools.domain.member.controller.response.MemberSearchResponse;
 import com.stool.studentcooperationtools.domain.member.service.MemberService;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -18,6 +20,8 @@ import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 import static org.springframework.restdocs.payload.JsonFieldType.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -31,7 +35,7 @@ public class MemberApiControllerDocsTest extends RestDocsSupport {
     }
 
     @Test
-    void searchFriend() throws Exception {
+    void findFriend() throws Exception {
         //given
         List<MemberFindMemberDto> memberDtoList = List.of(
                 MemberFindMemberDto.builder()
@@ -80,4 +84,121 @@ public class MemberApiControllerDocsTest extends RestDocsSupport {
                     )
                 );
     }
+
+    @Test
+    void searchNotFriend() throws Exception {
+                //given
+                List<MemberSearchMemberDto> memberDtoList = List.of(
+                        MemberSearchMemberDto.builder()
+                                .profile("profilePath")
+                                .email("email")
+                                .nickname("nickname")
+                                .build()
+                );
+                MemberSearchResponse response = MemberSearchResponse.builder()
+                        .members(memberDtoList)
+                        .num(memberDtoList.size())
+                        .build();
+
+                Mockito.when(memberService.searchFriend(Mockito.anyBoolean(),Mockito.anyString()))
+                        .thenReturn(response);
+
+                //when
+                //then
+                mockMvc.perform(
+                            MockMvcRequestBuilders.get("/api/v1/friends/search")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .param("relation","false")
+                                    .param("name","라이푸니")
+                        )
+                        .andDo(print())
+                        .andExpect(status().isOk())
+                        .andDo(document("friend-search-NotFriend",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                queryParameters(
+                                			parameterWithName("relation").description("조회할 유저가 친구 상태인지"),
+                                            parameterWithName("name").description("검색할 유저 이름")
+                                ),
+                                responseFields(
+                                        fieldWithPath("code").type(NUMBER)
+                                                .description("상태 코드"),
+                                        fieldWithPath("status").type(STRING)
+                                                .description("응답 상태"),
+                                        fieldWithPath("data").type(OBJECT)
+                                                .description("응답 데이터"),
+                                        fieldWithPath("data.num").type(NUMBER)
+                                                .description("데이터 개수"),
+                                        fieldWithPath("data.members[]").type(ARRAY)
+                                                .description("유저 정보 리스트"),
+                                        fieldWithPath("data.members[].email").type(STRING)
+                                                .description("유저 이메일"),
+                                        fieldWithPath("data.members[].nickname").type(STRING)
+                                                .description("유저 닉네임"),
+                                        fieldWithPath("data.members[].profile").type(STRING)
+                                                .description("유저 프로필")
+                                )
+                            )
+                        );
+
+    }
+
+    @Test
+    void searchFriend() throws Exception {
+                //given
+                List<MemberSearchMemberDto> memberDtoList = List.of(
+                        MemberSearchMemberDto.builder()
+                                .profile("profilePath")
+                                .email("email")
+                                .nickname("nickname")
+                                .build()
+                );
+                MemberSearchResponse response = MemberSearchResponse.builder()
+                        .members(memberDtoList)
+                        .num(memberDtoList.size())
+                        .build();
+
+                Mockito.when(memberService.searchFriend(Mockito.anyBoolean(),Mockito.anyString()))
+                        .thenReturn(response);
+
+                //when
+                //then
+                mockMvc.perform(
+                            MockMvcRequestBuilders.get("/api/v1/friends/search")
+                                    .contentType(MediaType.APPLICATION_JSON)
+                                    .param("relation","true")
+                                    .param("name","라이푸니")
+                        )
+                        .andDo(print())
+                        .andExpect(status().isOk())
+                        .andDo(document("friend-search",
+                                preprocessRequest(prettyPrint()),
+                                preprocessResponse(prettyPrint()),
+                                queryParameters(
+                                			parameterWithName("relation").description("조회할 유저가 친구 상태인지"),
+                                            parameterWithName("name").description("검색할 유저 이름")
+                                ),
+                                responseFields(
+                                        fieldWithPath("code").type(NUMBER)
+                                                .description("상태 코드"),
+                                        fieldWithPath("status").type(STRING)
+                                                .description("응답 상태"),
+                                        fieldWithPath("data").type(OBJECT)
+                                                .description("응답 데이터"),
+                                        fieldWithPath("data.num").type(NUMBER)
+                                                .description("데이터 개수"),
+                                        fieldWithPath("data.members[]").type(ARRAY)
+                                                .description("유저 정보 리스트"),
+                                        fieldWithPath("data.members[].email").type(STRING)
+                                                .description("유저 이메일"),
+                                        fieldWithPath("data.members[].nickname").type(STRING)
+                                                .description("유저 닉네임"),
+                                        fieldWithPath("data.members[].profile").type(STRING)
+                                                .description("유저 프로필")
+                                )
+                            )
+                        );
+            }
+
+
 }
