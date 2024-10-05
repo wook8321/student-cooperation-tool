35cacad94e076e47f2d7e7a5ed81d5578911276b
laipuni <2tgb02023@gmail.com>
2024-10-05 13:05:18 +0900

    [BackEnd] feat : 유저 검색 및 조회 Api 추가

--- src/main/java/com/stool/studentcooperationtools/domain/member/controller/MemberApiController.java
+++ src/main/java/com/stool/studentcooperationtools/domain/member/controller/MemberApiController.java
@@ -2,10 +2,12 @@ package com.stool.studentcooperationtools.domain.member.controller;
 
 import com.stool.studentcooperationtools.domain.api.ApiResponse;
 import com.stool.studentcooperationtools.domain.member.controller.response.MemberFindResponse;
+import com.stool.studentcooperationtools.domain.member.controller.response.MemberSearchResponse;
 import com.stool.studentcooperationtools.domain.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,6 +19,15 @@ public class MemberApiController {
     @GetMapping("/api/v1/friends")
     public ApiResponse<MemberFindResponse> findFriends(){
         MemberFindResponse response = memberService.findFriends();
+        return ApiResponse.of(HttpStatus.OK,response);
+    }
+
+    @GetMapping("/api/v1/friends/search")
+    public ApiResponse<MemberSearchResponse> searchFriends(
+            @RequestParam("relation") boolean relation,
+            @RequestParam("name") String name
+                                                           ){
+        MemberSearchResponse response = memberService.searchFriend(relation, name);
         return ApiResponse.of(HttpStatus.OK,response);
     }
 
