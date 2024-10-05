6eea90bca04b8f26ade8ace5d4d4f6fde94c1734
laipuni <2tgb02023@gmail.com>
2024-10-05 18:15:13 +0900

    [BackEnd] feat : RoomService 생성 및 참가한 방 조회 메소드 추가

---
+++ src/main/java/com/stool/studentcooperationtools/domain/room/service/RoomService.java
@@ -0,0 +1,11 @@
+package com.stool.studentcooperationtools.domain.room.service;
+
+import com.stool.studentcooperationtools.domain.room.controller.response.RoomsFindResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RoomService {
+    public RoomsFindResponse findRooms(final int page) {
+        return null;
+    }
+}
