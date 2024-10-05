2a508466a5c3a8dc0ed06f742bf12b9e46018227
laipuni <2tgb02023@gmail.com>
2024-10-05 19:06:20 +0900

    [BackEnd] feat : RoomApiController에 방 생성하기 Api 추가

--- src/main/java/com/stool/studentcooperationtools/domain/room/controller/RoomApiController.java
+++ src/main/java/com/stool/studentcooperationtools/domain/room/controller/RoomApiController.java
@@ -1,13 +1,13 @@
 package com.stool.studentcooperationtools.domain.room.controller;
 
 import com.stool.studentcooperationtools.domain.api.ApiResponse;
+import com.stool.studentcooperationtools.domain.room.controller.request.RoomAddRequest;
+import com.stool.studentcooperationtools.domain.room.controller.response.RoomAddResponse;
 import com.stool.studentcooperationtools.domain.room.controller.response.RoomsFindResponse;
 import com.stool.studentcooperationtools.domain.room.service.RoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,6 +18,12 @@ public class RoomApiController {
     @GetMapping("/api/v1/rooms")
     public ApiResponse<RoomsFindResponse> findRooms(@RequestParam("page") int page){
         RoomsFindResponse response = roomService.findRooms(page);
+        return ApiResponse.of(HttpStatus.OK,response);
+    }
+
+    @PostMapping("/api/v1/rooms")
+    public ApiResponse<RoomAddResponse> addRoom(@RequestBody RoomAddRequest request){
+        RoomAddResponse response = roomService.addRoom(request);
         return ApiResponse.of(HttpStatus.OK,response);
     }
 }
