c95237d40c487e0d4b02892b0c86bfdb41c90082
laipuni <2tgb02023@gmail.com>
2024-10-05 19:06:48 +0900

    [BackEnd] feat : RoomService에 방 생성 로직 추가

--- src/main/java/com/stool/studentcooperationtools/domain/room/service/RoomService.java
+++ src/main/java/com/stool/studentcooperationtools/domain/room/service/RoomService.java
@@ -1,5 +1,7 @@
 package com.stool.studentcooperationtools.domain.room.service;
 
+import com.stool.studentcooperationtools.domain.room.controller.request.RoomAddRequest;
+import com.stool.studentcooperationtools.domain.room.controller.response.RoomAddResponse;
 import com.stool.studentcooperationtools.domain.room.controller.response.RoomsFindResponse;
 import org.springframework.stereotype.Service;
 
@@ -7,5 +9,10 @@ import org.springframework.stereotype.Service;
 public class RoomService {
     public RoomsFindResponse findRooms(final int page) {
         return null;
+    }
+
+    public RoomAddResponse addRoom(final RoomAddRequest request) {
+        return null;
+
     }
 }
