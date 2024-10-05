093f46829bf16535f1e95db72497ff8387440030
laipuni <2tgb02023@gmail.com>
2024-10-05 13:05:45 +0900

    [BackEnd] feat : 유저 검색 및 조회 로직 추가

--- src/main/java/com/stool/studentcooperationtools/domain/member/service/MemberService.java
+++ src/main/java/com/stool/studentcooperationtools/domain/member/service/MemberService.java
@@ -1,11 +1,16 @@
 package com.stool.studentcooperationtools.domain.member.service;
 
 import com.stool.studentcooperationtools.domain.member.controller.response.MemberFindResponse;
+import com.stool.studentcooperationtools.domain.member.controller.response.MemberSearchResponse;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MemberService {
     public MemberFindResponse findFriends() {
+        return null;
+    }
+
+    public MemberSearchResponse searchFriend(final boolean relation, final String name) {
         return null;
     }
 }
