70439604fb705b69f47adc050613fc7c2b83edff
laipuni <2tgb02023@gmail.com>
2024-10-05 14:22:52 +0900

    [BackEnd] feat : MemberService 친구 추가 로직 생성

--- src/main/java/com/stool/studentcooperationtools/domain/member/service/MemberService.java
+++ src/main/java/com/stool/studentcooperationtools/domain/member/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.stool.studentcooperationtools.domain.member.service;
 
+import com.stool.studentcooperationtools.domain.member.controller.request.MemberAddRequest;
 import com.stool.studentcooperationtools.domain.member.controller.response.MemberFindResponse;
 import com.stool.studentcooperationtools.domain.member.controller.response.MemberSearchResponse;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,10 @@ public class MemberService {
     }
 
     public MemberSearchResponse searchFriend(final boolean relation, final String name) {
+        return null;
+    }
+
+    public Boolean addFriend(final MemberAddRequest request) {
         return null;
     }
 }
