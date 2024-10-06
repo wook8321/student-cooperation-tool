e55115c02fccbbac72d9a2b496e3fdcc56768f1e
laipuni <2tgb02023@gmail.com>
2024-10-06 12:48:48 +0900

    [BackEnd] feat : PartService 생성 및 자료 조사 역할들 조회 기능메소드 추가

---
+++ src/main/java/com/stool/studentcooperationtools/domain/part/service/PartService.java
@@ -0,0 +1,11 @@
+package com.stool.studentcooperationtools.domain.part.service;
+
+import com.stool.studentcooperationtools.domain.part.controller.response.PartFindResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PartService {
+    public PartFindResponse findParts(final Long roomId) {
+        return null;
+    }
+}
