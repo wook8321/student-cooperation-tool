aecd25ecbe181ea1fe0e8546f034380c1e0c666b
laipuni <2tgb02023@gmail.com>
2024-10-06 16:48:34 +0900

    [BackEnd] feat : ReviewService 생성 및 조사 자료의 평가 조회 기능 추가

---
+++ src/main/java/com/stool/studentcooperationtools/domain/review/service/ReviewService.java
@@ -0,0 +1,12 @@
+package com.stool.studentcooperationtools.domain.review.service;
+
+import com.stool.studentcooperationtools.domain.review.controller.response.ReviewFindResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+    public ReviewFindResponse findReview(final Long partId) {
+        return null;
+    }
+
+}
