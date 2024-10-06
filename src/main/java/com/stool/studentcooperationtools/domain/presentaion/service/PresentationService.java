679baa106ad89deb293cf12982e00019b63de710
laipuni <2tgb02023@gmail.com>
2024-10-06 20:30:18 +0900

    [BackEnd] feat : PresentationService 생성 및 해당 방의 발표자료를 조회 메소드 추가

---
+++ src/main/java/com/stool/studentcooperationtools/domain/presentaion/service/PresentationService.java
@@ -0,0 +1,11 @@
+package com.stool.studentcooperationtools.domain.presentaion.service;
+
+import com.stool.studentcooperationtools.domain.presentaion.controller.response.PresentationFindResponse;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PresentationService {
+    public PresentationFindResponse findPresentation(final Long roomId) {
+        return null;
+    }
+}
