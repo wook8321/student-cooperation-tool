59dadea5757a1211d7cdc9715b22f3f584634865
laipuni <2tgb02023@gmail.com>
2024-10-06 17:23:11 +0900

    [BackEnd] feat : 해당 자료에 평가를 등록 Api 추사

--- src/main/java/com/stool/studentcooperationtools/domain/review/controller/ReviewApiController.java
+++ src/main/java/com/stool/studentcooperationtools/domain/review/controller/ReviewApiController.java
@@ -1,13 +1,13 @@
 package com.stool.studentcooperationtools.domain.review.controller;
 
 import com.stool.studentcooperationtools.domain.api.ApiResponse;
+import com.stool.studentcooperationtools.domain.review.controller.request.ReviewAddRequest;
+import com.stool.studentcooperationtools.domain.review.controller.response.ReviewAddResponse;
 import com.stool.studentcooperationtools.domain.review.controller.response.ReviewFindResponse;
 import com.stool.studentcooperationtools.domain.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +19,12 @@ public class ReviewApiController {
     public ApiResponse<ReviewFindResponse> findReviews(@PathVariable("partId") Long partId){
         ReviewFindResponse response = reviewService.findReview(partId);
         return ApiResponse.of(HttpStatus.OK,response);
+    }
+
+    @PostMapping("/api/v1/parts/review")
+    public ApiResponse<ReviewAddResponse> addReviews(@RequestBody ReviewAddRequest request){
+        ReviewAddResponse response = reviewService.addReview(request);
+        return ApiResponse.of(HttpStatus.OK, response);
     }
 
 }
