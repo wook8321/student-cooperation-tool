7ef2f5bbe3e1e610e1e3d5fdfb65ab96229b04c1
laipuni <2tgb02023@gmail.com>
2024-10-09 14:49:02 +0900

    [BackEnd] feat : Participation 생성
    1:N과 N:1의 관계를 풀기위해 만들어졌고, 방과 참여자의 외래키를 가지는 엔티티다.

---
+++ src/main/java/com/stool/studentcooperationtools/domain/participation/Participation.java
@@ -0,0 +1,32 @@
+package com.stool.studentcooperationtools.domain.participation;
+
+import com.stool.studentcooperationtools.domain.BaseTimeEntity;
+import com.stool.studentcooperationtools.domain.member.Member;
+import com.stool.studentcooperationtools.domain.room.Room;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Participation extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Room room;
+
+    @Builder
+    private Participation(final Member member, final Room room) {
+        this.member = member;
+        this.room = room;
+    }
+}
