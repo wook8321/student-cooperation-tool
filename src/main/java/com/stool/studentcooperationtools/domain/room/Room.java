2f6b02e8f646c825dd045c5e206c26f0d6675bd4
laipuni <2tgb02023@gmail.com>
2024-10-09 15:18:59 +0900

    [BackEnd] feat : Room에 participationList 생성자연관관계 메소드 추가
    Participation을 추가할 리스트와 연관관계 메소드, 생성자 추가

--- src/main/java/com/stool/studentcooperationtools/domain/room/Room.java
+++ src/main/java/com/stool/studentcooperationtools/domain/room/Room.java
@@ -1,11 +1,16 @@
 package com.stool.studentcooperationtools.domain.room;
 
 import com.stool.studentcooperationtools.domain.BaseTimeEntity;
+import com.stool.studentcooperationtools.domain.participation.Participation;
 import com.stool.studentcooperationtools.domain.topic.Topic;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -22,14 +27,27 @@ public class Room extends BaseTimeEntity {
     @Column(nullable = false, length = 20)
     private String password;
 
+    @OneToMany(mappedBy = "room")
+    private List<Participation> participationList = new ArrayList<>();
+
     @Column
     private int participationNum;
 
     @OneToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
     private Topic mainTopic;
 
+    @Builder
+    private Room(final String title, final String password, final int participationNum) {
+        this.title = title;
+        this.password = password;
+        this.participationNum = participationNum;
+    }
+    
     public String getTopic(){
         return this.mainTopic.getTopic();
     }
 
+    public void addParticipation(Participation participation){
+        participationList.add(participation);
+    }
 }
