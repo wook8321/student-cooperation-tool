9dbe41f8b80b497fd8051b7b7d825e11725a6779
pyunye <dpwns2318@naver.com>
2024-10-11 01:37:10 +0900

    [BackEnd] refactor : MemberService refactoring
    Optional<List> -> List & 기타 코드 리팩토링

--- src/main/java/com/stool/studentcooperationtools/domain/member/service/MemberService.java
+++ src/main/java/com/stool/studentcooperationtools/domain/member/service/MemberService.java
@@ -12,11 +12,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -24,41 +23,32 @@ public class MemberService {
 
     //유저의 친구 목록 조회
     public MemberFindResponse findFriends() {
+        //todo user의 id 파라미터 입력
         List<Member> friends = memberRepository.findFriendsByMemberId(1L);
         return MemberFindResponse.of(friends);
     }
 
     //relation에 따라 "nickName"과 친구 관계거나 아닌 유저들 조회
     public MemberSearchResponse searchFriend(final boolean relation, final String nickName) {
+        //todo user의 id 파라미터 입력
         if (relation){
-            Optional<List<Member>> findFriends = memberRepository.findFriendsByMemberNickName(nickName);
-            if(findFriends.isEmpty()){
-                throw new IllegalArgumentException("해당 검색어와 일치하는 유저가 존재하지 않습니다.");
-            }
-            return MemberSearchResponse.of(findFriends.get());
+            List<Member> findFriends = memberRepository.findFriendsByMemberNickName(nickName, 1L);
+            return MemberSearchResponse.of(findFriends);
         }
         else{
-            Optional<List<Member>> findNonFriends = memberRepository.findNonFriendsByMemberNickName(nickName);
-            if(findNonFriends.isEmpty()){
-                throw new IllegalArgumentException("해당 검색어와 일치하는 유저가 존재하지 않습니다.");
-            }
-            return MemberSearchResponse.of(findNonFriends.get());
+            List<Member> findNonFriends = memberRepository.findNonFriendsByMemberNickName(nickName, 1L);
+            return MemberSearchResponse.of(findNonFriends);
         }
     }
 
-    //친구추가 - 1. 검색한 email을 가진 멤버 조회 2. 이미 친구인지 조회 3. friendship 엔티티 만들어서 등록
+    //친구추가 - 1. 검색한 email을 가진 멤버 조회 2. friendship 엔티티 만들어서 등록
+    @Transactional
     public Boolean addFriend(final MemberAddRequest request) {
+        //todo 본인 member 엔티티 조회
         String findEmail = request.getEmail();
-        Optional<Member> optionalMember = memberRepository.findMemberByEmail(findEmail);
-        if (optionalMember.isEmpty()){
-            throw new IllegalArgumentException("해당 유저가 존재하지 않습니다.");
-        }
-        Member newFriend = optionalMember.get();
-        boolean existsFriendship = friendshipRepository.existsByMemberIdAndFriendId(1L, newFriend.getId());
-        if (existsFriendship){
-            throw new IllegalArgumentException("이미 친구 관계의 유저입니다.");
-        }
-        friendshipRepository.save(Friendship.of(newFriend, newFriend)); //첫 파라미터 유저로 변경 필요
+        Member friend = memberRepository.findMemberByEmail(findEmail)
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저가 존재하지 않습니다."));
+        friendshipRepository.save(Friendship.of(friend, friend)); //첫 파라미터 유저로 변경 필요
         return true;
     }
 }
