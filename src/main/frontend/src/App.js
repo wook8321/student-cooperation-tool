1ffa0ffbab2800e470bd772f05d46581837c3aa5
laipuni <2tgb02023@gmail.com>
2024-10-11 17:35:18 +0900

    [FrontEnd] design : App.js에 테스트 용 소셜 로그인, 로그아웃 추가

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -7,9 +7,9 @@ function App() {
   const [hello, setHello] = useState('');
 
   useEffect(() => {
-    axios.get(domain + '/api/test')
+    axios.get(domain + '/api/loginTest')
         .then(res => {
-          setHello(res.data);
+          setHello(JSON.stringify(res.data));
         })
         .catch(error =>{
             alert(JSON.stringify(error))
@@ -18,6 +18,11 @@ function App() {
   return (
       <div className="App">
         백엔드 데이터 : {hello}
+          <a className="btn btn-primary w-100 py-2 mb-1" href="/oauth2/authorization/google" type="button">Google로 로그인</a>
+          <form action="/logout" method="post">
+            <button type={"submit"}> 로그아웃</button>
+          </form>
+          {/*로그아웃은 post 방식으로 <form>테그로 csrf 토큰과 함께 요청하면 됨*/}
       </div>
   );
 }
