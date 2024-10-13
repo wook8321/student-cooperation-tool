9cb26390871118ee64e7cfc17e295fc20ca23fee
laipuni <2tgb02023@gmail.com>
2024-10-13 18:32:16 +0900

    [BackEnd] feat : TopicRepository에 해당 방의 주제들을 조회하는 기능 추가

--- src/main/java/com/stool/studentcooperationtools/domain/topic/repository/TopicRepository.java
+++ src/main/java/com/stool/studentcooperationtools/domain/topic/repository/TopicRepository.java
@@ -2,6 +2,14 @@ package com.stool.studentcooperationtools.domain.topic.repository;
 
 import com.stool.studentcooperationtools.domain.topic.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TopicRepository extends JpaRepository<Topic,Long> {
+    @Query("select t from Topic t join t.member join t.room " +
+            "where t.room.id = :roomId " +
+            "order by t.createdTime desc")
+    List<Topic> findAllByRoomId(@Param("roomId") Long roomId);
 }
