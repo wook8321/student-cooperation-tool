5937396b63cf0d338d055b58b090984a7d737ca6
laipuni <2tgb02023@gmail.com>
2024-10-13 17:47:32 +0900

    [BackEnd] feat : TopicWebsocketController에 주제를 삭제하는 웹소켓 api 추가

--- src/main/java/com/stool/studentcooperationtools/websocket/controller/topic/TopicWebsocketController.java
+++ src/main/java/com/stool/studentcooperationtools/websocket/controller/topic/TopicWebsocketController.java
@@ -2,12 +2,13 @@ package com.stool.studentcooperationtools.websocket.controller.topic;
 
 import com.stool.studentcooperationtools.domain.topic.service.TopicService;
 import com.stool.studentcooperationtools.security.oauth2.dto.SessionMember;
+import com.stool.studentcooperationtools.websocket.controller.Utils.SimpleMessageSendingUtils;
 import com.stool.studentcooperationtools.websocket.controller.topic.request.TopicAddSocketRequest;
+import com.stool.studentcooperationtools.websocket.controller.topic.request.TopicDeleteSocketRequest;
 import com.stool.studentcooperationtools.websocket.controller.topic.response.TopicAddSocketResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -16,15 +17,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class TopicWebsocketController {
 
     private final TopicService topicService;
-    private final SimpMessageSendingOperations simpMessageSendingOperations;
-
+    private final SimpleMessageSendingUtils sendingUtils;
     @MessageMapping("/topics/add")
     public void addTopic(@Valid @RequestBody TopicAddSocketRequest request, SessionMember member){
         TopicAddSocketResponse response = topicService.addTopic(request, member.getMemberSeq());
-        simpMessageSendingOperations.convertAndSend(
-                String.format("/sub/rooms/%d/topics",request.getRoomId()),
-                response
-        );
+        sendingUtils.convertAndSend(sendingUtils.createTopicDecisionSubUrl(request.getRoomId()), response);
+    }
+
+    @MessageMapping("/topics/delete")
+    public void deleteTopic(@Valid @RequestBody TopicDeleteSocketRequest request){
+        Boolean result = topicService.deleteTopic(request);
+        sendingUtils.convertAndSend(sendingUtils.createTopicDecisionSubUrl(request.getRoomId()),result);
     }
 
 }
