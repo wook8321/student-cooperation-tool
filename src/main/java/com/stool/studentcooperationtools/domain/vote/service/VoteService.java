5ac8c6f4a486e5a8ec8fc12325ede7c9fd43abe8
laipuni <2tgb02023@gmail.com>
2024-10-13 17:51:38 +0900

    [BackEnd] feat : VoteService 생성 및 주제에 투표 기능 추가

---
+++ src/main/java/com/stool/studentcooperationtools/domain/vote/service/VoteService.java
@@ -0,0 +1,34 @@
+package com.stool.studentcooperationtools.domain.vote.service;
+
+import com.stool.studentcooperationtools.domain.member.Member;
+import com.stool.studentcooperationtools.domain.member.repository.MemberRepository;
+import com.stool.studentcooperationtools.domain.topic.Topic;
+import com.stool.studentcooperationtools.domain.topic.repository.TopicRepository;
+import com.stool.studentcooperationtools.domain.vote.Vote;
+import com.stool.studentcooperationtools.domain.vote.respository.VoteRepository;
+import com.stool.studentcooperationtools.websocket.controller.vote.request.VoteAddWebSocketRequest;
+import com.stool.studentcooperationtools.websocket.controller.vote.response.VoteAddWebSocketResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class VoteService {
+
+    private final VoteRepository voteRepository;
+    private final MemberRepository memberRepository;
+    private final TopicRepository topicRepository;
+
+    @Transactional
+    public VoteAddWebSocketResponse addVote(final VoteAddWebSocketRequest request, final Long memberSeq) {
+        Member member = memberRepository.findById(memberSeq)
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저는 존재하지 않습니다."));
+        Topic topic = topicRepository.findById(request.getTopicId())
+                .orElseThrow(() -> new IllegalArgumentException("투표할 주제가 존재하지 않습니다."));
+        Vote vote = Vote.of(member, topic);
+        voteRepository.save(vote);
+        return VoteAddWebSocketResponse.of(vote,member);
+    }
+}
