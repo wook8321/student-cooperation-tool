348a1eca65b81015e8bd1364fdadbd065de41698
laipuni <2tgb02023@gmail.com>
2024-10-13 15:41:30 +0900

    [BackEnd] feat : TopicService에 주제를 등록하는 기능을 추가

--- src/main/java/com/stool/studentcooperationtools/domain/topic/service/TopicService.java
+++ src/main/java/com/stool/studentcooperationtools/domain/topic/service/TopicService.java
@@ -1,11 +1,43 @@
 package com.stool.studentcooperationtools.domain.topic.service;
 
+import com.stool.studentcooperationtools.domain.member.Member;
+import com.stool.studentcooperationtools.domain.member.repository.MemberRepository;
+import com.stool.studentcooperationtools.domain.room.Room;
+import com.stool.studentcooperationtools.domain.room.repository.RoomRepository;
+import com.stool.studentcooperationtools.domain.topic.Topic;
 import com.stool.studentcooperationtools.domain.topic.controller.response.TopicFindResponse;
+import com.stool.studentcooperationtools.domain.topic.repository.TopicRepository;
+import com.stool.studentcooperationtools.websocket.controller.topic.request.TopicAddSocketRequest;
+import com.stool.studentcooperationtools.websocket.controller.topic.response.TopicAddSocketResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class TopicService {
+
+    private final MemberRepository memberRepository;
+    private final RoomRepository roomRepository;
+    private final TopicRepository topicRepository;
+
     public TopicFindResponse findTopics(final Long roomId) {
         return null;
+    }
+
+    @Transactional
+    public TopicAddSocketResponse addTopic(final TopicAddSocketRequest request, final Long memberSeq) {
+        Member member = memberRepository.findById(memberSeq)
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저는 존재하지 않습니다"));
+        Room room = roomRepository.findById(request.getRoomId())
+                .orElseThrow(() -> new IllegalArgumentException("해당 방은 존재하지 않습니다."));
+        Topic topic = Topic.builder()
+                .topic(request.getTopic())
+                .member(member)
+                .room(room)
+                .build();
+        topicRepository.save(topic);
+        return TopicAddSocketResponse.of(topic);
     }
 }
