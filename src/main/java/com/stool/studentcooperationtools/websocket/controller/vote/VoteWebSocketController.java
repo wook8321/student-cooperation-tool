660891729f0632a2737b422b1d28e27f7bd1f68b
laipuni <2tgb02023@gmail.com>
2024-10-13 18:02:09 +0900

    [BackEnd] feat : VoteWebSocketController에 주제의 투표 삭제 Api 추가

--- src/main/java/com/stool/studentcooperationtools/websocket/controller/vote/VoteWebSocketController.java
+++ src/main/java/com/stool/studentcooperationtools/websocket/controller/vote/VoteWebSocketController.java
@@ -4,6 +4,7 @@ import com.stool.studentcooperationtools.domain.vote.service.VoteService;
 import com.stool.studentcooperationtools.security.oauth2.dto.SessionMember;
 import com.stool.studentcooperationtools.websocket.controller.Utils.SimpleMessageSendingUtils;
 import com.stool.studentcooperationtools.websocket.controller.vote.request.VoteAddWebSocketRequest;
+import com.stool.studentcooperationtools.websocket.controller.vote.request.VoteDeleteSocketRequest;
 import com.stool.studentcooperationtools.websocket.controller.vote.response.VoteAddWebSocketResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -17,11 +18,17 @@ public class VoteWebSocketController {
 
     private final VoteService voteService;
     private final SimpleMessageSendingUtils sendingUtils;
+
     @MessageMapping("/votes/add")
     public void addVote(@Valid @RequestBody VoteAddWebSocketRequest request, SessionMember member){
         VoteAddWebSocketResponse response = voteService.addVote(request, member.getMemberSeq());
         sendingUtils.convertAndSend(sendingUtils.createTopicDecisionSubUrl(request.getRoomId()),response);
     }
 
+    @MessageMapping("/votes/delete")
+    public void deleteVote(@Valid @RequestBody VoteDeleteSocketRequest request, SessionMember member){
+        Boolean result = voteService.deleteVote(request.getVoteId());
+        sendingUtils.convertAndSend(sendingUtils.createTopicDecisionSubUrl(request.getRoomId()),request);
+    }
 
 }
