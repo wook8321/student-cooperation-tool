5d6e599d2fe21874d2990c075dda53f2acd4720e
laipuni <2tgb02023@gmail.com>
2024-10-13 15:42:15 +0900

    [BackEnd] feat : RoomRepository 생성

---
+++ src/main/java/com/stool/studentcooperationtools/domain/room/repository/RoomRepository.java
@@ -0,0 +1,8 @@
+package com.stool.studentcooperationtools.domain.room.repository;
+
+
+import com.stool.studentcooperationtools.domain.room.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Long> {
+}
