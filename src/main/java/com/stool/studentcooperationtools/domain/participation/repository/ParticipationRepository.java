836a7de28a59b8578ea9f1a3c338dfbbd545dd51
pyunye <dpwns2318@naver.com>
2024-10-14 19:18:30 +0900

    [BackEnd] feat : ParticipationRepository 방의 유저 유무 메소드 구현

--- src/main/java/com/stool/studentcooperationtools/domain/participation/repository/ParticipationRepository.java
+++ src/main/java/com/stool/studentcooperationtools/domain/participation/repository/ParticipationRepository.java
@@ -2,8 +2,11 @@ package com.stool.studentcooperationtools.domain.participation.repository;
 
 import com.stool.studentcooperationtools.domain.participation.Participation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ParticipationRepository extends JpaRepository<Participation, Long> {
+
+    Boolean existsByMemberIdAndRoomId(Long memberId, Long roomId);
 }
