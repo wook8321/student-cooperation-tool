f49719ef8110d13253af06c34b92c1a1775bb686
pyunye <dpwns2318@naver.com>
2024-10-15 13:50:02 +0900

    [BackEnd] refactor : RoomRepository exists 쿼리문 수정

--- src/main/java/com/stool/studentcooperationtools/domain/room/repository/RoomRepository.java
+++ src/main/java/com/stool/studentcooperationtools/domain/room/repository/RoomRepository.java
@@ -22,6 +22,6 @@ public interface RoomRepository extends JpaRepository<Room, Long> {
     @Query("select r from Room r join Participation p on p.member.id = :memberId where p.room.id = :roomId")
     Optional<Room> findRoomByRoomId(@Param("memberId") Long memberId, @Param("roomId") Long roomId);
 
-    @Query("select case when count(r) > 0 then true else false end from Room r join Participation p on p.member.id = :memberId where r.title like %:title%")
+    @Query("select exists(r) from Room r join Participation p on p.member.id = :memberId where r.title like %:title%")
     Boolean existsByTitle(@Param("memberId") Long memberId, @Param("title") String title);
 }
