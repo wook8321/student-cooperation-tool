fbdacbb300772998b3f152e794e6c4d6b229e695
laipuni <2tgb02023@gmail.com>
2024-10-17 21:23:58 +0900

    [BackEnd] feat : WebsocketSecurityInterceptor에 db에 저장된 세션값 조회 로직 추가
    세션을 In-memory 방식이 아닌 RDBMS에 저장하는 방식으로 바뀌어 DB에 session을 저장하는 JdbcIndexedSessionRepository를 참조해서 세션을 조회하는 로직 추가했다. 만약 DB에도 세션이 없을 경우 웹소켓 연결을 할 수 없다.

--- src/main/java/com/stool/studentcooperationtools/websocket/interceptor/WebsocketSecurityInterceptor.java
+++ src/main/java/com/stool/studentcooperationtools/websocket/interceptor/WebsocketSecurityInterceptor.java
@@ -1,6 +1,5 @@
 package com.stool.studentcooperationtools.websocket.interceptor;
 
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.Ordered;
@@ -13,6 +12,7 @@ import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.session.SessionAuthenticationException;
+import org.springframework.session.jdbc.JdbcIndexedSessionRepository;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -21,23 +21,22 @@ import org.springframework.stereotype.Component;
 @Order(Ordered.HIGHEST_PRECEDENCE + 99)
 public class WebsocketSecurityInterceptor implements ChannelInterceptor {
 
-    private final HttpSession httpSession;
-
+    private final JdbcIndexedSessionRepository jdbcIndexedSessionRepository;
     @Override
     public Message<?> preSend(final Message<?> message, final MessageChannel channel) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
-        if (SimpMessageType.CONNECT.equals(accessor.getMessageType()) ||
-                SimpMessageType.MESSAGE.equals(accessor.getMessageType()) ||
-                SimpMessageType.SUBSCRIBE.equals(accessor.getMessageType())) {
+        if (SimpMessageType.CONNECT.equals(accessor.getMessageType())) {
             Authentication authentication = SecurityContextHolder.getContextHolderStrategy().getContext().getAuthentication();
             if(authentication == null){
                 // 웹소켓 연결 전에 인증하지 않은 경우 authentication는 조회되지 않음
-                throw new SessionAuthenticationException("Authentication Failed");
+                String jsessionid = accessor.getFirstNativeHeader("JSESSIONID");
+                Object session = jdbcIndexedSessionRepository.findById(jsessionid);
+                if(session == null){
+                    throw new SessionAuthenticationException("Authentication Failed");
+                }
             }
-
-            accessor.setUser(authentication);
         }
 
-        return ChannelInterceptor.super.preSend(message, channel);
+        return message;
     }
 }
