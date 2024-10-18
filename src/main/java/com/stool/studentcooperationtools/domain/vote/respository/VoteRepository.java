e0dae37302e5965959043a49e2e931e4637ff2ec
laipuni <2tgb02023@gmail.com>
2024-10-18 19:35:04 +0900

    [BackEnd] feat : VoteRepository에 deleteVoteByIdAndDeleterId 추가
    deleteVoteByIdAndDeleterId() : 투표의 id와 삭제하는 유저의 id로 삭제하는 기능

--- src/main/java/com/stool/studentcooperationtools/domain/vote/respository/VoteRepository.java
+++ src/main/java/com/stool/studentcooperationtools/domain/vote/respository/VoteRepository.java
@@ -2,6 +2,14 @@ package com.stool.studentcooperationtools.domain.vote.respository;
 
 import com.stool.studentcooperationtools.domain.vote.Vote;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface VoteRepository extends JpaRepository<Vote,Long> {
+
+    @Modifying
+    @Query(value = "delete from Vote v where v.voter.id = :deleterId and v.id = :voteId")
+    Boolean deleteVoteByIdAndDeleterId(@Param("voteId") Long voteId, @Param("deleterId") Long deleterId);
+
 }
