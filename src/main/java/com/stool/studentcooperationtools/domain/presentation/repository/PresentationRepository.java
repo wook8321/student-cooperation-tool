a6418072bfb509dd0378ad6be2070f4aa7ce45e6
pyunye <dpwns2318@naver.com>
2024-11-01 21:32:39 +0900

    [Backend] feat : PresentationRepository exist, update 메소드 추가 (exist)해당 방에 ppt가 존재하는지 확인 (update)해당 방 ppt의 주소 변경

--- src/main/java/com/stool/studentcooperationtools/domain/presentation/repository/PresentationRepository.java
+++ src/main/java/com/stool/studentcooperationtools/domain/presentation/repository/PresentationRepository.java
@@ -2,7 +2,9 @@ package com.stool.studentcooperationtools.domain.presentation.repository;
 
 import com.stool.studentcooperationtools.domain.presentation.Presentation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +13,12 @@ import java.util.Optional;
 public interface PresentationRepository extends JpaRepository<Presentation, Long> {
 
     Optional<Presentation> findByRoomId(Long roomId);
+
+    Boolean existsByRoomId(Long roomId);
+
+    @Modifying
+    @Query(value = "update Presentation p set " +
+            "p.presentationPath = :presentationPath " +
+            "where p.room.leader.id = :updaterId")
+    int updatePresentationByLeader(@Param("presentationPath") String presentationPath, @Param("updaterId") Long updaterId);
 }
