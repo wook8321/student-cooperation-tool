f3c0231473c55c4dd0f9995c48af3b61f0af6ee5
laipuni <2tgb02023@gmail.com>
2024-11-01 18:21:32 +0900

    [BackEnd] feat : ChatService에 findChats 구현
    findChats는 조회할 채팅이 속하는 방의 식별키 roomId 받고, 조회할 채팅의 page를 받는다. chatRepository를 이용해서 받은 채팅들을 조회한 값을 ChatFindResponse 감싸서 controller에 보낸다.

--- src/main/java/com/stool/studentcooperationtools/domain/chat/service/ChatService.java
+++ src/main/java/com/stool/studentcooperationtools/domain/chat/service/ChatService.java
@@ -1,11 +1,21 @@
 package com.stool.studentcooperationtools.domain.chat.service;
 
 import com.stool.studentcooperationtools.domain.chat.controller.response.ChatFindResponse;
+import com.stool.studentcooperationtools.domain.chat.repository.ChatRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class ChatService {
-    public ChatFindResponse findChats(final Long roomId) {
-        return null;
+
+    private final ChatRepository chatRepository;
+
+    public ChatFindResponse findChats(final Long roomId,final int page) {
+        PageRequest pageRequest = PageRequest.of(page, 10);
+        return ChatFindResponse.of(
+                chatRepository.findChatsByIdAndSlicingASC(roomId,pageRequest)
+        );
     }
 }
