b102903e11e634f200abdac9db5b36cf6631af0f
laipuni <2tgb02023@gmail.com>
2024-11-01 20:36:26 +0900

    [BackEnd] feat : ChatService에 addChat추가
    addChat는 채팅을 추가 요청을 받아 채팅을 추가하는 서비스 단 로직이다.

--- src/main/java/com/stool/studentcooperationtools/domain/chat/service/ChatService.java
+++ src/main/java/com/stool/studentcooperationtools/domain/chat/service/ChatService.java
@@ -1,15 +1,27 @@
 package com.stool.studentcooperationtools.domain.chat.service;
 
+import com.stool.studentcooperationtools.domain.chat.Chat;
 import com.stool.studentcooperationtools.domain.chat.controller.response.ChatFindResponse;
 import com.stool.studentcooperationtools.domain.chat.repository.ChatRepository;
+import com.stool.studentcooperationtools.domain.member.Member;
+import com.stool.studentcooperationtools.domain.member.repository.MemberRepository;
+import com.stool.studentcooperationtools.domain.room.Room;
+import com.stool.studentcooperationtools.domain.room.repository.RoomRepository;
+import com.stool.studentcooperationtools.security.oauth2.dto.SessionMember;
+import com.stool.studentcooperationtools.websocket.controller.chat.request.ChatAddWebsocketRequest;
+import com.stool.studentcooperationtools.websocket.controller.chat.response.ChatAddWebsocketResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class ChatService {
 
+    private final RoomRepository roomRepository;
+    private final MemberRepository memberRepository;
     private final ChatRepository chatRepository;
 
     public ChatFindResponse findChats(final Long roomId,final int page) {
@@ -17,5 +29,17 @@ public class ChatService {
         return ChatFindResponse.of(
                 chatRepository.findChatsByIdAndSlicingASC(roomId,pageRequest)
         );
+    }
+
+    @Transactional
+    public ChatAddWebsocketResponse addChat(final ChatAddWebsocketRequest request, final SessionMember sessionMember) {
+        Member member = memberRepository.findById(sessionMember.getMemberSeq())
+                .orElseThrow(() -> new IllegalArgumentException("채팅 등록에 실패했습니다."));
+
+        Room room = roomRepository.findById(request.getRoomId())
+                .orElseThrow(() -> new IllegalArgumentException("채팅을 등록할 방이 존재하지 않습니다."));
+
+        Chat chat = Chat.of(request.getContent(), member,room);
+        return ChatAddWebsocketResponse.of(chatRepository.save(chat));
     }
 }
