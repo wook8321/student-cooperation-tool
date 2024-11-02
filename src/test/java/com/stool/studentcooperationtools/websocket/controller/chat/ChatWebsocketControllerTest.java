c645b4d180851dd87ef5c5413528b5b651f327c7
laipuni <2tgb02023@gmail.com>
2024-11-02 14:00:49 +0900

    [BackEnd] test : ChatWebsocketControllerTest에 deleteChat 테스트 추가

--- src/test/java/com/stool/studentcooperationtools/websocket/controller/chat/ChatWebsocketControllerTest.java
+++ src/test/java/com/stool/studentcooperationtools/websocket/controller/chat/ChatWebsocketControllerTest.java
@@ -5,7 +5,9 @@ import com.stool.studentcooperationtools.security.oauth2.dto.SessionMember;
 import com.stool.studentcooperationtools.websocket.WebsocketMessageType;
 import com.stool.studentcooperationtools.websocket.WebsocketTestSupport;
 import com.stool.studentcooperationtools.websocket.controller.chat.request.ChatAddWebsocketRequest;
+import com.stool.studentcooperationtools.websocket.controller.chat.request.ChatDeleteWebsocketRequest;
 import com.stool.studentcooperationtools.websocket.controller.chat.response.ChatAddWebsocketResponse;
+import com.stool.studentcooperationtools.websocket.controller.chat.response.ChatDeleteWebsocketResponse;
 import com.stool.studentcooperationtools.websocket.controller.request.WebsocketResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,7 +25,7 @@ class ChatWebsocketControllerTest extends WebsocketTestSupport {
     @MockBean
     private ChatService chatService;
 
-    @DisplayName("채팅 추가 요청을 받아서 채팅을 등록 후 응답 한다.")
+    @DisplayName("채팅 추가 요청을 받아서 채팅을 등록 후 응답한다.")
     @Test
     void addChat() throws ExecutionException, InterruptedException, TimeoutException {
         //given
@@ -55,6 +57,37 @@ class ChatWebsocketControllerTest extends WebsocketTestSupport {
         //then
         assertThat(stompSession.isConnected()).isTrue();
         assertThat(websocketResponse.getMessageType()).isEqualTo(WebsocketMessageType.CHAT_ADD);
+        assertThat(websocketResponse.getData()).isNotNull();
+    }
+
+    @DisplayName("채팅 삭제 요청을 받아서 채팅을 삭제 후 응답한다.")
+    @Test
+    void deleteChat() throws ExecutionException, InterruptedException, TimeoutException {
+        //given
+        Long roomId = 1L;
+        String chatSubUrl = "/sub/rooms/"+ roomId +"/chat";
+        Long chatId = 1L;
+
+        ChatDeleteWebsocketRequest request = ChatDeleteWebsocketRequest.builder()
+                .roomId(roomId)
+                .chatId(chatId)
+                .build();
+
+        ChatDeleteWebsocketResponse response = new ChatDeleteWebsocketResponse(chatId);
+
+        Mockito.when(chatService.deleteChat(
+                Mockito.any(ChatDeleteWebsocketRequest.class),Mockito.any(SessionMember.class)
+        )).thenReturn(response);
+
+        stompSession.subscribe(chatSubUrl,resultHandler);
+
+        //when
+        stompSession.send("/pub/chats/delete",request);
+        WebsocketResponse websocketResponse = resultHandler.get(3);
+
+        //then
+        assertThat(stompSession.isConnected()).isTrue();
+        assertThat(websocketResponse.getMessageType()).isEqualTo(WebsocketMessageType.CHAT_DELETE);
         assertThat(websocketResponse.getData()).isNotNull();
     }
 
