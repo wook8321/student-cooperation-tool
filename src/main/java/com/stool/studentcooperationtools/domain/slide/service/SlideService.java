56613efb9221bf29d25c54d3cac7848c24417def
pyunye <dpwns2318@naver.com>
2024-11-03 00:56:49 +0900

    [Backend] refactor : SlideRepository findSlides -> findSlidesAndScripts fetch join을 통해 script 정보 같이 가져오기

--- src/main/java/com/stool/studentcooperationtools/domain/slide/service/SlideService.java
+++ src/main/java/com/stool/studentcooperationtools/domain/slide/service/SlideService.java
@@ -1,5 +1,7 @@
 package com.stool.studentcooperationtools.domain.slide.service;
 
+import com.stool.studentcooperationtools.domain.script.Script;
+import com.stool.studentcooperationtools.domain.script.repository.ScriptRepository;
 import com.stool.studentcooperationtools.domain.slide.Slide;
 import com.stool.studentcooperationtools.domain.slide.controller.response.SlideFindResponse;
 import com.stool.studentcooperationtools.domain.slide.repository.SlideRepository;
@@ -12,8 +14,9 @@ import java.util.List;
 @RequiredArgsConstructor
 public class SlideService {
     private final SlideRepository slideRepository;
+
     public SlideFindResponse findSlides(final Long presentationId) {
-        List<Slide> slides = slideRepository.findByPresentationId(presentationId);
+        List<Slide> slides = slideRepository.findSlidesAndScriptsByPresentationId(presentationId);
         return SlideFindResponse.of(slides);
     }
 }
--- src/test/java/com/stool/studentcooperationtools/domain/slide/repository/SlideRepositoryTest.java
+++ src/test/java/com/stool/studentcooperationtools/domain/slide/repository/SlideRepositoryTest.java
@@ -3,7 +3,10 @@ package com.stool.studentcooperationtools.domain.slide.repository;
 import com.stool.studentcooperationtools.domain.presentation.Presentation;
 import com.stool.studentcooperationtools.domain.presentation.repository.PresentationRepository;
 import com.stool.studentcooperationtools.domain.room.repository.RoomRepository;
+import com.stool.studentcooperationtools.domain.script.Script;
+import com.stool.studentcooperationtools.domain.script.repository.ScriptRepository;
 import com.stool.studentcooperationtools.domain.slide.Slide;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -20,25 +24,36 @@ class SlideRepositoryTest {
     private SlideRepository slideRepository;
     @Autowired
     private PresentationRepository presentationRepository;
+    @Autowired
+    private ScriptRepository scriptRepository;
 
     @Test
-    @DisplayName("ppt의 유효한 슬라이드 찾기")
+    @DisplayName("ppt의 유효한 슬라이드와 스크립트 찾기")
     void findSlidesByPresentationId() {
         //given
         Presentation presentation = Presentation.builder()
                 .presentationPath("path")
                 .build();
         presentationRepository.save(presentation);
+        Script script = Script.builder()
+                .script("s")
+                .presentation(presentation)
+                .build();
+        scriptRepository.save(script);
         Slide slide = Slide.builder()
                 .slideUrl("url")
                 .presentation(presentation)
                 .thumbnail("thumbnail")
+                .script(script)
                 .build();
         slideRepository.save(slide);
         //when
-        List<Slide> slides = slideRepository.findByPresentationId(presentation.getId());
+        List<Slide> slides = slideRepository.findSlidesAndScriptsByPresentationId(presentation.getId());
+        Slide findSlide = slides.get(0);
         //then
         assertEquals(slides.size(), 1);
+        assertEquals(findSlide.getSlideUrl(), "url");
+        assertEquals(findSlide.getScript().getId(), script.getId());
     }
 
 
@@ -51,7 +66,7 @@ class SlideRepositoryTest {
                 .build();
         presentationRepository.save(presentation);
         //when
-        List<Slide> slides = slideRepository.findByPresentationId(presentation.getId());
+        List<Slide> slides = slideRepository.findSlidesAndScriptsByPresentationId(presentation.getId());
         //then
         assertEquals(slides.size(), 0);
     }
