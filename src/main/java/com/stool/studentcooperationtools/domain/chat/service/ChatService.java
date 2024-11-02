754b262450034cb095f4b94ffc5f6b27235d46ff
laipuni <2tgb02023@gmail.com>
2024-11-02 14:03:26 +0900

    [BackEnd] feat : ChatService에 deleteChat추가
    deleteChat는 댓글을 삭제하는 유저를 조회해서 채팅의 주인이 아닐 경우 삭제를 못하고, 맞는 경우 댓글을 삭제하고 결과를 반환한다.

--- src/main/java/com/stool/studentcooperationtools/domain/chat/service/ChatService.java
+++ src/main/java/com/stool/studentcooperationtools/domain/chat/service/ChatService.java
@@ -9,7 +9,9 @@ import com.stool.studentcooperationtools.domain.room.Room;
 import com.stool.studentcooperationtools.domain.room.repository.RoomRepository;
 import com.stool.studentcooperationtools.security.oauth2.dto.SessionMember;
 import com.stool.studentcooperationtools.websocket.controller.chat.request.ChatAddWebsocketRequest;
+import com.stool.studentcooperationtools.websocket.controller.chat.request.ChatDeleteWebsocketRequest;
 import com.stool.studentcooperationtools.websocket.controller.chat.response.ChatAddWebsocketResponse;
+import com.stool.studentcooperationtools.websocket.controller.chat.response.ChatDeleteWebsocketResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -41,5 +43,20 @@ public class ChatService {
 
         Chat chat = Chat.of(request.getContent(), member,room);
         return ChatAddWebsocketResponse.of(chatRepository.save(chat));
+    }
+
+    @Transactional
+    public ChatDeleteWebsocketResponse deleteChat(final ChatDeleteWebsocketRequest request, final SessionMember member) {
+        Chat chat = chatRepository.findById(request.getChatId())
+                .orElseThrow(() -> new IllegalArgumentException("삭제할 채팅이 존재하지 않습니다."));
+        if(hasNotAuthorizationBy(member, chat)){
+            throw new IllegalArgumentException("채팅을 제거할 권한이 없습니다.");
+        }
+        chatRepository.deleteById(request.getChatId());
+        return new ChatDeleteWebsocketResponse(request.getChatId());
+    }
+
+    private static boolean hasNotAuthorizationBy(final SessionMember member, final Chat chat) {
+        return !chat.getMember().getId().equals(member.getMemberSeq());
     }
 }
