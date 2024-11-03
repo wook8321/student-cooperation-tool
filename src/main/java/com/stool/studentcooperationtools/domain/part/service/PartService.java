ba66cab4b4978ee33cf0e9f448fbd7a9c741a256
laipuni <2tgb02023@gmail.com>
2024-11-03 16:30:24 +0900

    [BackEnd] feat : PartService에 findParts구현
    findParts는 조회할 part들이 속하는 방의 식별키(roomId)를 받아 조회한 뒤, Dto로 감싸서 반환한다.
    .

--- src/main/java/com/stool/studentcooperationtools/domain/part/service/PartService.java
+++ src/main/java/com/stool/studentcooperationtools/domain/part/service/PartService.java
@@ -1,11 +1,23 @@
 package com.stool.studentcooperationtools.domain.part.service;
 
+import com.stool.studentcooperationtools.domain.part.Part;
 import com.stool.studentcooperationtools.domain.part.controller.response.PartFindResponse;
+import com.stool.studentcooperationtools.domain.part.repository.PartRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class PartService {
+
+    private final PartRepository partRepository;
+
     public PartFindResponse findParts(final Long roomId) {
-        return null;
+        List<Part> parts = partRepository.findAllByRoomId(roomId);
+        return PartFindResponse.of(parts);
     }
 }
