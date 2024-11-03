ad588c5399359e7a59cf6035ffb926afab011bea
pyunye <dpwns2318@naver.com>
2024-11-03 23:33:59 +0900

    [Backend] feat : GoogleCredentialArgumentResolver 구현 +)WebConfig에 반영

--- src/main/java/com/stool/studentcooperationtools/web/config/WebConfig.java
+++ src/main/java/com/stool/studentcooperationtools/web/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.stool.studentcooperationtools.web.config;
 
+import com.stool.studentcooperationtools.security.credential.resolver.GoogleCredentialArgumentResolver;
 import com.stool.studentcooperationtools.web.resolver.SessionMemberArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
@@ -13,10 +14,12 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
 
     private final SessionMemberArgumentResolver sessionMemberArgumentResolver;
+    private final GoogleCredentialArgumentResolver googleCredentialArgumentResolver;
 
     @Override
     public void addArgumentResolvers(final List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(sessionMemberArgumentResolver);
+        resolvers.add(googleCredentialArgumentResolver);
         WebMvcConfigurer.super.addArgumentResolvers(resolvers);
     }
 }
