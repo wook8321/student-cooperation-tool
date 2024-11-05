5a023013c540fc2809b06d25409896168e11ae59
laipuni <2tgb02023@gmail.com>
2024-11-05 18:42:57 +0900

    [BackEnd] feat : PartServiceTest에 addPart 테스트

--- src/test/java/com/stool/studentcooperationtools/domain/part/service/PartServiceTest.java
+++ src/test/java/com/stool/studentcooperationtools/domain/part/service/PartServiceTest.java
@@ -8,13 +8,19 @@ import com.stool.studentcooperationtools.domain.part.controller.response.PartFin
 import com.stool.studentcooperationtools.domain.part.repository.PartRepository;
 import com.stool.studentcooperationtools.domain.room.Room;
 import com.stool.studentcooperationtools.domain.room.repository.RoomRepository;
+import com.stool.studentcooperationtools.security.oauth2.dto.SessionMember;
+import com.stool.studentcooperationtools.websocket.controller.part.request.PartAddWebsocketRequest;
+import com.stool.studentcooperationtools.websocket.controller.part.response.PartAddWebsocketResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @Transactional
@@ -69,4 +75,102 @@ class PartServiceTest {
                 .containsExactly(content);
     }
 
+    @DisplayName("역할을 추가할 때, 역할 추가 요청을 한 유저가 존재하지 않을 경우 에러가 발생한다.")
+    @Test
+    void addPartWithNotExistMember(){
+        //given
+        Long invalidRoomId = 1L;
+        Long invalidMemberId = 1L;
+        String invalidProfile = "프로필";
+        String invalidNickname = "닉네임";
+        String partName = "조사할 부분";
+        PartAddWebsocketRequest request = PartAddWebsocketRequest.builder()
+                .roomId(invalidRoomId)
+                .partName(partName)
+                .build();
+
+        SessionMember sessionMember = SessionMember.builder()
+                .memberSeq(invalidMemberId)
+                .profile(invalidProfile)
+                .nickName(invalidNickname)
+                .build();
+
+        //when
+        //then
+        assertThatThrownBy(() -> partService.addPart(request, sessionMember))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageMatching("역할을 추가하는 것을 실패했습니다.");
+    }
+
+
+    @DisplayName("역할을 추가할 때, 역할을 추가할 방이 존재하지 않는다면 에러가 발생한다.")
+    @Test
+    void addPartWithNotExistRoom(){
+        //given
+        Member member = Member.builder()
+                .role(Role.USER)
+                .email("email")
+                .profile("profile")
+                .nickName("nickname")
+                .build();
+        memberRepository.save(member);
+
+        String partName = "조사할 부분";
+        Long invalidRoomId = 1L;
+        PartAddWebsocketRequest request = PartAddWebsocketRequest.builder()
+                .roomId(invalidRoomId)
+                .partName(partName)
+                .build();
+
+        SessionMember sessionMember = SessionMember.builder()
+                .memberSeq(member.getId())
+                .profile(member.getProfile())
+                .nickName(member.getNickName())
+                .build();
+
+        //when
+        //then
+        assertThatThrownBy(() -> partService.addPart(request, sessionMember))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageMatching("역할을 추가할 방이 존재하지 않습니다.");
+    }
+
+    @DisplayName("역할을 역할 요청을 받아 역할을 추가한다.")
+    @Test
+    void addPart(){
+        //given
+        Member member = Member.builder()
+                .role(Role.USER)
+                .email("email")
+                .profile("profile")
+                .nickName("nickname")
+                .build();
+        memberRepository.save(member);
+        Room room = Room.builder()
+                .leader(member)
+                .password("password")
+                .participationNum(1)
+                .build();
+        roomRepository.save(room);
+
+        String partName = "조사할 부분";
+        PartAddWebsocketRequest request = PartAddWebsocketRequest.builder()
+                .roomId(room.getId())
+                .partName(partName)
+                .build();
+
+        SessionMember sessionMember = SessionMember.builder()
+                .memberSeq(member.getId())
+                .profile(member.getProfile())
+                .nickName(member.getNickName())
+                .build();
+
+        //when
+        PartAddWebsocketResponse response = partService.addPart(request, sessionMember);
+        List<Part> result = partRepository.findAll();
+        //then
+        assertThat(response).isNotNull()
+                .extracting("partId","partName")
+                .containsExactlyInAnyOrder(result.get(0).getId(),partName);
+    }
 }
