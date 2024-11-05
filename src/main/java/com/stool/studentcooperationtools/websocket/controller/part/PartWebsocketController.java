b1e0d0e17c869d1f8def8aa66f5acdcf4750da26
laipuni <2tgb02023@gmail.com>
2024-11-05 18:49:26 +0900

    [BackEnd] feat : PartWebsocketController 생성 및 addPart 추가
    PartWebsocketController는 Part와 관련된 Websocket 요청을 받아 처리하는 controller이다. addPart는 역할을 추가하는 요청을 받아 응답하는 핸들러이다.

---
+++ src/main/java/com/stool/studentcooperationtools/websocket/controller/part/PartWebsocketController.java
@@ -0,0 +1,33 @@
+package com.stool.studentcooperationtools.websocket.controller.part;
+
+import com.stool.studentcooperationtools.domain.part.service.PartService;
+import com.stool.studentcooperationtools.security.oauth2.dto.SessionMember;
+import com.stool.studentcooperationtools.websocket.controller.Utils.SimpleMessageSendingUtils;
+import com.stool.studentcooperationtools.websocket.controller.part.request.PartAddWebsocketRequest;
+import com.stool.studentcooperationtools.websocket.controller.part.response.PartAddWebsocketResponse;
+import com.stool.studentcooperationtools.websocket.controller.request.WebsocketResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import static com.stool.studentcooperationtools.websocket.WebsocketMessageType.PART_ADD;
+
+@Controller
+@RequiredArgsConstructor
+public class PartWebsocketController {
+
+    private final PartService partService;
+    private final SimpleMessageSendingUtils sendingUtils;
+
+    @MessageMapping("/parts/add")
+    public void addPart(@Valid @RequestBody PartAddWebsocketRequest request, SessionMember member){
+        PartAddWebsocketResponse response = partService.addPart(request,member);
+        sendingUtils.convertAndSend(
+                sendingUtils.creatPartResearchSubUrl(request.getRoomId()),
+                WebsocketResponse.of(PART_ADD,response)
+        );
+    }
+
+}
