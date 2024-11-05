48544dc5bd656794b116f283dd6d01f3a0fb30ec
laipuni <2tgb02023@gmail.com>
2024-11-05 20:45:00 +0900

    [BackEnd] feat : PartRepository에 deletePartByLeaderOrOwner 추가
    deletePartByLeaderOrOwner는 방장 혹은 역할을 만든 본인이 역할을 삭제한다.

--- src/main/java/com/stool/studentcooperationtools/domain/part/repository/PartRepository.java
+++ src/main/java/com/stool/studentcooperationtools/domain/part/repository/PartRepository.java
@@ -2,6 +2,7 @@ package com.stool.studentcooperationtools.domain.part.repository;
 
 import com.stool.studentcooperationtools.domain.part.Part;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -11,5 +12,11 @@ public interface PartRepository extends JpaRepository<Part,Long> {
 
     @Query("select p from Part p join fetch p.room r join fetch p.member where r.id = :roomId")
     List<Part> findAllByRoomId(@Param("roomId") Long roomId);
+
+    @Modifying
+    @Query("delete Part p " +
+            "where p.id = :partId and " +
+            "(p.member.id = :deleterId or p.room.leader.id = :deleterId)")
+    int deletePartByLeaderOrOwner(@Param("partId")Long partId,@Param("deleterId")Long deleterId);
 
 }
