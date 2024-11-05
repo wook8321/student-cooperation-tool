61f2910c98599137aa36088b7962ce8a7b3aace3
laipuni <2tgb02023@gmail.com>
2024-11-05 18:42:39 +0900

    [BackEnd] feat : PartService에 addPart
    addPart는 조사할 부분 역할을 추가한다. 역할을 추가하는 유저가 존재,로그인 하지 않거나 추가할 방이 없다면 에러가 발생한다.

--- src/main/java/com/stool/studentcooperationtools/domain/part/service/PartService.java
+++ src/main/java/com/stool/studentcooperationtools/domain/part/service/PartService.java
@@ -1,8 +1,15 @@
 package com.stool.studentcooperationtools.domain.part.service;
 
+import com.stool.studentcooperationtools.domain.member.Member;
+import com.stool.studentcooperationtools.domain.member.repository.MemberRepository;
 import com.stool.studentcooperationtools.domain.part.Part;
 import com.stool.studentcooperationtools.domain.part.controller.response.PartFindResponse;
 import com.stool.studentcooperationtools.domain.part.repository.PartRepository;
+import com.stool.studentcooperationtools.domain.room.Room;
+import com.stool.studentcooperationtools.domain.room.repository.RoomRepository;
+import com.stool.studentcooperationtools.security.oauth2.dto.SessionMember;
+import com.stool.studentcooperationtools.websocket.controller.part.request.PartAddWebsocketRequest;
+import com.stool.studentcooperationtools.websocket.controller.part.response.PartAddWebsocketResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,9 +22,27 @@ import java.util.List;
 public class PartService {
 
     private final PartRepository partRepository;
+    private final MemberRepository memberRepository;
+    private final RoomRepository roomRepository;
 
     public PartFindResponse findParts(final Long roomId) {
         List<Part> parts = partRepository.findAllByRoomId(roomId);
         return PartFindResponse.of(parts);
+    }
+
+    @Transactional
+    public PartAddWebsocketResponse addPart(final PartAddWebsocketRequest request, final SessionMember sessionMember) {
+        Member member = memberRepository.findById(sessionMember.getMemberSeq())
+                .orElseThrow(() -> new IllegalArgumentException("역할을 추가하는 것을 실패했습니다."));
+
+        Room room = roomRepository.findById(request.getRoomId())
+                .orElseThrow(() -> new IllegalArgumentException("역할을 추가할 방이 존재하지 않습니다."));
+
+        Part part = Part.builder()
+                .partName(request.getPartName())
+                .member(member)
+                .room(room)
+                .build();
+        return PartAddWebsocketResponse.of(partRepository.save(part));
     }
 }
