4ae417b929aade0a07ad4d93f9c34108d58721ca
laipuni <2tgb02023@gmail.com>
2024-11-05 20:46:48 +0900

    [BackEnd] feat : PartService에  deletePart 추가

--- src/main/java/com/stool/studentcooperationtools/domain/part/service/PartService.java
+++ src/main/java/com/stool/studentcooperationtools/domain/part/service/PartService.java
@@ -9,8 +9,11 @@ import com.stool.studentcooperationtools.domain.room.Room;
 import com.stool.studentcooperationtools.domain.room.repository.RoomRepository;
 import com.stool.studentcooperationtools.security.oauth2.dto.SessionMember;
 import com.stool.studentcooperationtools.websocket.controller.part.request.PartAddWebsocketRequest;
+import com.stool.studentcooperationtools.websocket.controller.part.request.PartDeleteWebsocketRequest;
 import com.stool.studentcooperationtools.websocket.controller.part.response.PartAddWebsocketResponse;
+import com.stool.studentcooperationtools.websocket.controller.part.response.PartDeleteWebsocketResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -44,5 +47,14 @@ public class PartService {
                 .room(room)
                 .build();
         return PartAddWebsocketResponse.of(partRepository.save(part));
+    }
+
+    @Transactional(rollbackFor = AccessDeniedException.class)
+    public PartDeleteWebsocketResponse deletePart(final PartDeleteWebsocketRequest request, final SessionMember member) {
+        int result = partRepository.deletePartByLeaderOrOwner(request.getPartId(), member.getMemberSeq());
+        if(result == 0){
+            throw new AccessDeniedException("역할을 삭제할 권한이 없습니다.");
+        }
+        return PartDeleteWebsocketResponse.of(request.getPartId());
     }
 }
