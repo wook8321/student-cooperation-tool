622fac01180a25f252920d927de7fb1904e3b3f1
laipuni <2tgb02023@gmail.com>
2024-11-05 20:49:20 +0900

    [BackEnd] feat : PartDeleteWebsocketResponse 생성
    PartDeleteWebsocketResponse는 역할을 삭제하고 결과값을 저장하는 응답 값이다.

---
+++ src/main/java/com/stool/studentcooperationtools/websocket/controller/part/response/PartDeleteWebsocketResponse.java
@@ -0,0 +1,22 @@
+package com.stool.studentcooperationtools.websocket.controller.part.response;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PartDeleteWebsocketResponse {
+
+    private Long partId;
+
+    @Builder
+    private PartDeleteWebsocketResponse(final Long partId) {
+        this.partId = partId;
+    }
+
+    public static PartDeleteWebsocketResponse of(final Long partId) {
+        return null;
+    }
+}
