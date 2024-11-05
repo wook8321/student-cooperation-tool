51eaba4f2c16a5c218545d5cc3ec193e2d99d83d
laipuni <2tgb02023@gmail.com>
2024-11-05 20:50:48 +0900

    [BackEnd] feat : PartWebsocketController에 deletePart추가
    deletePart는 역할 삭제 요청을 받아 삭제 후, 결과 값을 자료 조사 역할에 구독한 유저들에게 전달한다.

--- src/main/java/com/stool/studentcooperationtools/websocket/controller/part/PartWebsocketController.java
+++ src/main/java/com/stool/studentcooperationtools/websocket/controller/part/PartWebsocketController.java
@@ -4,7 +4,9 @@ import com.stool.studentcooperationtools.domain.part.service.PartService;
 import com.stool.studentcooperationtools.security.oauth2.dto.SessionMember;
 import com.stool.studentcooperationtools.websocket.controller.Utils.SimpleMessageSendingUtils;
 import com.stool.studentcooperationtools.websocket.controller.part.request.PartAddWebsocketRequest;
+import com.stool.studentcooperationtools.websocket.controller.part.request.PartDeleteWebsocketRequest;
 import com.stool.studentcooperationtools.websocket.controller.part.response.PartAddWebsocketResponse;
+import com.stool.studentcooperationtools.websocket.controller.part.response.PartDeleteWebsocketResponse;
 import com.stool.studentcooperationtools.websocket.controller.request.WebsocketResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import static com.stool.studentcooperationtools.websocket.WebsocketMessageType.PART_ADD;
+import static com.stool.studentcooperationtools.websocket.WebsocketMessageType.PART_DELETE;
 
 @Controller
 @RequiredArgsConstructor
@@ -27,6 +30,15 @@ public class PartWebsocketController {
         sendingUtils.convertAndSend(
                 sendingUtils.creatPartResearchSubUrl(request.getRoomId()),
                 WebsocketResponse.of(PART_ADD,response)
+        );
+    }
+
+    @MessageMapping("/parts/delete")
+    public void deletePart(@Valid @RequestBody PartDeleteWebsocketRequest request, SessionMember member){
+        PartDeleteWebsocketResponse response = partService.deletePart(request,member);
+        sendingUtils.convertAndSend(
+                sendingUtils.creatPartResearchSubUrl(request.getRoomId()),
+                WebsocketResponse.of(PART_DELETE,response)
         );
     }
 
