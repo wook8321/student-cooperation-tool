cfae575bb3b1d237eaac3f6392ed8bf85068ac6a
laipuni <2tgb02023@gmail.com>
2024-11-06 19:42:28 +0900

    [BackEnd] feat : ReviewRepository에 deleteReviewByMemberIdAndReviewId추가
    deleteReviewByMemberIdAndReviewId 삭제할 리뷰와 삭제하는 유저의 id를 받아와서 리뷰를 삭제한다.

--- src/main/java/com/stool/studentcooperationtools/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/stool/studentcooperationtools/domain/review/repository/ReviewRepository.java
@@ -2,6 +2,7 @@ package com.stool.studentcooperationtools.domain.review.repository;
 
 import com.stool.studentcooperationtools.domain.review.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -12,4 +13,8 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
     @Query("select r from Review r where r.part.id = :partId order by r.id desc")
     List<Review> findAllByPartIdOrderByIdDesc(@Param("partId") Long partId);
 
+    @Modifying
+    @Query("delete Review r " +
+            "where r.id = :reviewId and r.member.id = :memberId")
+    int deleteReviewByMemberIdAndReviewId(@Param("reviewId") Long reviewId,@Param("memberId")Long memberId);
 }
