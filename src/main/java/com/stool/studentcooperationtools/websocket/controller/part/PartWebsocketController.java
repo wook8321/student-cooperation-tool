5276791cea3441223699a0a02594e204950b1720
laipuni <2tgb02023@gmail.com>
2024-11-06 10:44:34 +0900

    [BackEnd] feat : PartWebsocketController에 updatePart추가
    updatePart는 역할을 수정할 요청을 받아 처리 후 결과값을 같은 url을 구독하고 있는 다른 유저들에게 보낸다.

--- src/main/java/com/stool/studentcooperationtools/websocket/controller/part/PartWebsocketController.java
+++ src/main/java/com/stool/studentcooperationtools/websocket/controller/part/PartWebsocketController.java
@@ -5,8 +5,10 @@ import com.stool.studentcooperationtools.security.oauth2.dto.SessionMember;
 import com.stool.studentcooperationtools.websocket.controller.Utils.SimpleMessageSendingUtils;
 import com.stool.studentcooperationtools.websocket.controller.part.request.PartAddWebsocketRequest;
 import com.stool.studentcooperationtools.websocket.controller.part.request.PartDeleteWebsocketRequest;
+import com.stool.studentcooperationtools.websocket.controller.part.request.PartUpdateWebsocketRequest;
 import com.stool.studentcooperationtools.websocket.controller.part.response.PartAddWebsocketResponse;
 import com.stool.studentcooperationtools.websocket.controller.part.response.PartDeleteWebsocketResponse;
+import com.stool.studentcooperationtools.websocket.controller.part.response.PartUpdateWebsocketResponse;
 import com.stool.studentcooperationtools.websocket.controller.request.WebsocketResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -14,8 +16,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import static com.stool.studentcooperationtools.websocket.WebsocketMessageType.PART_ADD;
-import static com.stool.studentcooperationtools.websocket.WebsocketMessageType.PART_DELETE;
+import static com.stool.studentcooperationtools.websocket.WebsocketMessageType.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -39,6 +40,16 @@ public class PartWebsocketController {
         sendingUtils.convertAndSend(
                 sendingUtils.creatPartResearchSubUrl(request.getRoomId()),
                 WebsocketResponse.of(PART_DELETE,response)
+        );
+    }
+
+
+    @MessageMapping("/parts/update")
+    public void updatePart(@Valid @RequestBody PartUpdateWebsocketRequest request, SessionMember member){
+        PartUpdateWebsocketResponse response = partService.updatePart(request,member);
+        sendingUtils.convertAndSend(
+                sendingUtils.creatPartResearchSubUrl(request.getRoomId()),
+                WebsocketResponse.of(PART_UPDATE,response)
         );
     }
 
