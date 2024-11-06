9c4593d016e28efcc6f738da3c7415bf3baf1204
laipuni <2tgb02023@gmail.com>
2024-11-06 11:00:29 +0900

    [BackEnd] feat : PartWebsocketControllerTest에 updatePart 테스트

--- src/test/java/com/stool/studentcooperationtools/websocket/controller/part/PartWebsocketControllerTest.java
+++ src/test/java/com/stool/studentcooperationtools/websocket/controller/part/PartWebsocketControllerTest.java
@@ -5,8 +5,10 @@ import com.stool.studentcooperationtools.security.oauth2.dto.SessionMember;
 import com.stool.studentcooperationtools.websocket.WebsocketTestSupport;
 import com.stool.studentcooperationtools.websocket.controller.part.request.PartAddWebsocketRequest;
 import com.stool.studentcooperationtools.websocket.controller.part.request.PartDeleteWebsocketRequest;
+import com.stool.studentcooperationtools.websocket.controller.part.request.PartUpdateWebsocketRequest;
 import com.stool.studentcooperationtools.websocket.controller.part.response.PartAddWebsocketResponse;
 import com.stool.studentcooperationtools.websocket.controller.part.response.PartDeleteWebsocketResponse;
+import com.stool.studentcooperationtools.websocket.controller.part.response.PartUpdateWebsocketResponse;
 import com.stool.studentcooperationtools.websocket.controller.request.WebsocketResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,8 +19,7 @@ import java.time.LocalDate;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
-import static com.stool.studentcooperationtools.websocket.WebsocketMessageType.PART_ADD;
-import static com.stool.studentcooperationtools.websocket.WebsocketMessageType.PART_DELETE;
+import static com.stool.studentcooperationtools.websocket.WebsocketMessageType.*;
 import static com.stool.studentcooperationtools.websocket.config.WebsocketConfig.PART_RESEARCH_URL_FORMAT;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,7 +28,7 @@ class PartWebsocketControllerTest extends WebsocketTestSupport {
     @MockBean
     PartService partService;
 
-    @DisplayName("웹소켓 방식온 해당 조사 역할을 추가 요청을 받고 같은 url에 구독한 유저들에게 보낸다.")
+    @DisplayName("웹소켓으로 해당 조사 역할을 추가 요청을 받고 같은 url에 구독한 유저들에게 보낸다.")
     @Test
     void addPart() throws ExecutionException, InterruptedException, TimeoutException {
         //given
@@ -57,7 +58,7 @@ class PartWebsocketControllerTest extends WebsocketTestSupport {
         assertThat(websocketResponse.getData()).isNotNull();
     }
 
-    @DisplayName("웹소켓 방식온 해당 조사 역할을 추가 요청을 받고 같은 url에 구독한 유저들에게 보낸다.")
+    @DisplayName("웹소켓으로 해당 조사 역할을 추가 요청을 받고 같은 url에 구독한 유저들에게 결과를 보낸다.")
     @Test
     void deletePart() throws ExecutionException, InterruptedException, TimeoutException {
         //given
@@ -83,5 +84,46 @@ class PartWebsocketControllerTest extends WebsocketTestSupport {
         //then
         assertThat(result.getMessageType()).isEqualTo(PART_DELETE);
         assertThat(result.getData()).isNotNull();
+    }
+
+    @DisplayName("웹소켓으로 역할 수정 요청을 받아서 같은 url에 구독한 유저들에게 결과를 보낸다")
+    @Test
+    void updatePart() throws ExecutionException, InterruptedException, TimeoutException {
+        //given
+        Long roomId = 1L;
+        Long partId = 1L;
+        String partName = "partName";
+        PartUpdateWebsocketRequest request = PartUpdateWebsocketRequest.builder()
+                .partId(partId)
+                .partName(partName)
+                .memberId(1L)
+                .roomId(roomId)
+                .build();
+
+        PartUpdateWebsocketResponse response = PartUpdateWebsocketResponse.builder()
+                .partId(partId)
+                .partName(partName)
+                .profile("profile")
+                .nickName("nickName")
+                .build();
+
+        Mockito.when(partService.updatePart(
+                Mockito.any(PartUpdateWebsocketRequest.class),Mockito.any(SessionMember.class)
+        )).thenReturn(response);
+
+        stompSession.subscribe(PART_RESEARCH_URL_FORMAT.formatted(roomId),resultHandler);
+        //when
+        stompSession.send("/pub/parts/update",request);
+        WebsocketResponse result = resultHandler.get(3);
+        //then
+        assertThat(result.getMessageType()).isEqualTo(PART_UPDATE);
+        assertThat(result.getData()).isNotNull()
+                .extracting("partId","partName","profile","nickName")
+                .containsExactlyInAnyOrder(
+                        response.getPartId().intValue(),
+                        response.getPartName(),
+                        response.getProfile(),
+                        response.getNickName()
+                );
     }
 }
