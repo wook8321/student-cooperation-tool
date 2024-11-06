b9df4c6b3c782867396ff5440c04b58f69a0b055
laipuni <2tgb02023@gmail.com>
2024-11-06 12:02:45 +0900

    [BackEnd] feat : ReviewService에 findReview 구현
    findReview는 조사할 부분에 평가들을 최신순으로 조회한다.

--- src/main/java/com/stool/studentcooperationtools/domain/review/service/ReviewService.java
+++ src/main/java/com/stool/studentcooperationtools/domain/review/service/ReviewService.java
@@ -1,14 +1,26 @@
 package com.stool.studentcooperationtools.domain.review.service;
 
+import com.stool.studentcooperationtools.domain.review.Review;
 import com.stool.studentcooperationtools.domain.review.controller.request.ReviewAddRequest;
 import com.stool.studentcooperationtools.domain.review.controller.response.ReviewAddResponse;
 import com.stool.studentcooperationtools.domain.review.controller.response.ReviewFindResponse;
+import com.stool.studentcooperationtools.domain.review.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+
     public ReviewFindResponse findReview(final Long partId) {
-        return null;
+        List<Review> result = reviewRepository.findAllByPartIdOrderByIdDesc(partId);
+        return ReviewFindResponse.of(result);
     }
 
     public ReviewAddResponse addReview(final ReviewAddRequest request) {
