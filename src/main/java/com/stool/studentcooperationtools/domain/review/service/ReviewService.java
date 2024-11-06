d7b09f1629e1e4db9d5e4f1647208d626703001f
laipuni <2tgb02023@gmail.com>
2024-11-06 19:45:01 +0900

    [BackEnd] feat : ReviewService에 deleteReview추가
    deleteReview는 리뷰 삭체 요청값들과 삭제하는 유저의 정보로 리뷰를 삭제한다. 만약 삭제하지 못했다면 접근권한이 없다는 에러가 발생한다.

--- src/main/java/com/stool/studentcooperationtools/domain/review/service/ReviewService.java
+++ src/main/java/com/stool/studentcooperationtools/domain/review/service/ReviewService.java
@@ -6,11 +6,13 @@ import com.stool.studentcooperationtools.domain.part.Part;
 import com.stool.studentcooperationtools.domain.part.repository.PartRepository;
 import com.stool.studentcooperationtools.domain.review.Review;
 import com.stool.studentcooperationtools.domain.review.controller.request.ReviewAddRequest;
+import com.stool.studentcooperationtools.domain.review.controller.request.ReviewDeleteRequest;
 import com.stool.studentcooperationtools.domain.review.controller.response.ReviewAddResponse;
 import com.stool.studentcooperationtools.domain.review.controller.response.ReviewFindResponse;
 import com.stool.studentcooperationtools.domain.review.repository.ReviewRepository;
 import com.stool.studentcooperationtools.security.oauth2.dto.SessionMember;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -44,5 +46,15 @@ public class ReviewService {
                 .member(member)
                 .build();
         return ReviewAddResponse.of(reviewRepository.save(review));
+    }
+
+    @Transactional(rollbackFor = AccessDeniedException.class)
+    public Boolean deleteReview(final ReviewDeleteRequest request, final SessionMember sessionMember) {
+        int result = reviewRepository.deleteReviewByMemberIdAndReviewId(request.getReviewId(), sessionMember.getMemberSeq());
+        if(result == 0){
+            //본인이 작성하지 않아 삭제하지 못한경우
+            throw new AccessDeniedException("평가를 제거할 권한이 없습니다.");
+        }
+        return true;
     }
 }
