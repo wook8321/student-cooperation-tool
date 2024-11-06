fba74120cf4ae5a0430afc1fdbdc03685d593054
pyunye <dpwns2318@naver.com>
2024-11-06 14:01:13 +0900

    [Backend] feat : GoogleCredentialProvider - drive api 사용 위해 httprequestinitializer 생성자 구현

--- src/main/java/com/stool/studentcooperationtools/security/credential/GoogleCredentialProvider.java
+++ src/main/java/com/stool/studentcooperationtools/security/credential/GoogleCredentialProvider.java
@@ -5,11 +5,16 @@ import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.slides.v1.SlidesScopes;
+import com.google.auth.Credentials;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -22,21 +27,30 @@ import java.util.List;
 @Component
 public class GoogleCredentialProvider {
     private final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-    private final List<String> SCOPES = Collections.singletonList(SlidesScopes.PRESENTATIONS_READONLY);
+    private final List<String> SCOPES = Collections.singletonList(SlidesScopes.PRESENTATIONS);
 
-    @Value("${google.slides.credentials-file-path}")
-    private String credentialsFilePath;
+    private final String credentialsFilePath;
 
-    @Value("${google.slides.tokens-directory-path}")
-    private String tokensDirectoryPath;
+    private final String tokensDirectoryPath;
 
-    @Value("${google.slides.user-id}")
-    private String userId;
+    private final String userId;
+
+    private final String credentialsforupdateFilePath;
 
     private Credential credential;
 
-    public GoogleCredentialProvider() throws IOException {
+    private HttpRequestInitializer requestInitializer;
+
+    public GoogleCredentialProvider(@Value("${google.slides.credentials-file-path}") String credentialsFilePath,
+                                    @Value("${google.slides.tokens-directory-path}") String tokensDirectoryPath,
+                                    @Value("${google.slides.user-id}") String userId,
+                                    @Value("${google.slides.credentials-forupdate-file-path}") String credentialsforupdateFilePath) throws IOException {
+        this.credentialsFilePath = credentialsFilePath;
+        this.tokensDirectoryPath = tokensDirectoryPath;
+        this.userId = userId;
+        this.credentialsforupdateFilePath = credentialsforupdateFilePath;
         this.credential = authorize();
+        this.requestInitializer = getHttpRequestInitializer();
     }
 
     private Credential authorize() throws IOException {
@@ -53,6 +67,14 @@ public class GoogleCredentialProvider {
                 .build();
         LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize(userId);
+    }
+
+    private HttpRequestInitializer getHttpRequestInitializer() throws IOException {
+        InputStream in = GoogleCredentialProvider.class.getResourceAsStream(credentialsFilePath);
+        GoogleCredentials credentials = GoogleCredentials.fromStream(in)
+                .createScoped(List.of(DriveScopes.DRIVE_FILE));
+        return new HttpCredentialsAdapter(
+                credentials);
     }
 
 }
