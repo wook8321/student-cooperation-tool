d57b599a92121c849a118b2a5cdd678bf8f67de1
laipuni <2tgb02023@gmail.com>
2024-11-06 12:47:38 +0900

    [BackEnd] feat : ReviewService에 addReview 구현
    addReview는 역할의 평가를 등록하는데, 유효하지 않은 유저정보,역할 정보를 받으면 에러가 발생한다.

--- src/main/java/com/stool/studentcooperationtools/domain/review/service/ReviewService.java
+++ src/main/java/com/stool/studentcooperationtools/domain/review/service/ReviewService.java
@@ -1,10 +1,15 @@
 package com.stool.studentcooperationtools.domain.review.service;
 
+import com.stool.studentcooperationtools.domain.member.Member;
+import com.stool.studentcooperationtools.domain.member.repository.MemberRepository;
+import com.stool.studentcooperationtools.domain.part.Part;
+import com.stool.studentcooperationtools.domain.part.repository.PartRepository;
 import com.stool.studentcooperationtools.domain.review.Review;
 import com.stool.studentcooperationtools.domain.review.controller.request.ReviewAddRequest;
 import com.stool.studentcooperationtools.domain.review.controller.response.ReviewAddResponse;
 import com.stool.studentcooperationtools.domain.review.controller.response.ReviewFindResponse;
 import com.stool.studentcooperationtools.domain.review.repository.ReviewRepository;
+import com.stool.studentcooperationtools.security.oauth2.dto.SessionMember;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,13 +22,27 @@ import java.util.List;
 public class ReviewService {
 
     private final ReviewRepository reviewRepository;
+    private final MemberRepository memberRepository;
+    private final PartRepository partRepository;
 
     public ReviewFindResponse findReview(final Long partId) {
         List<Review> result = reviewRepository.findAllByPartIdOrderByIdDesc(partId);
         return ReviewFindResponse.of(result);
     }
 
-    public ReviewAddResponse addReview(final ReviewAddRequest request) {
-        return null;
+    @Transactional
+    public ReviewAddResponse addReview(final ReviewAddRequest request, final SessionMember sessionMember) {
+        Member member = memberRepository.findById(sessionMember.getMemberSeq())
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저는 존재하지 않습니다."));
+
+        Part part = partRepository.findById(request.getPartId())
+                .orElseThrow(() -> new IllegalArgumentException("평가를 등록할 역할이 존재하지 않습니다."));
+
+        Review review = Review.builder()
+                .content(request.getContent())
+                .part(part)
+                .member(member)
+                .build();
+        return ReviewAddResponse.of(reviewRepository.save(review));
     }
 }
