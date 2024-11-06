ba72bc13e2a222f50313059cac7f0d4633973a99
pyunye <dpwns2318@naver.com>
2024-11-06 14:02:52 +0900

    [Backend] feat : SlidesFactory - driveService 생성자 구현

--- src/main/java/com/stool/studentcooperationtools/domain/slide/SlidesFactory.java
+++ src/main/java/com/stool/studentcooperationtools/domain/slide/SlidesFactory.java
@@ -1,21 +1,42 @@
 package com.stool.studentcooperationtools.domain.slide;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.slides.v1.Slides;
 import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.slides.v1.SlidesScopes;
+import com.google.auth.http.HttpCredentialsAdapter;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.stool.studentcooperationtools.security.credential.GoogleCredentialProvider;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.GeneralSecurityException;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Component
 public class SlidesFactory {
+    @Value("${google.slides.credentials-forupdate-file-path}")
+    private String credentialsFilePath;
 
     public Slides createSlidesService(Credential credential) throws GeneralSecurityException, IOException {
         final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
         return new Slides.Builder(HTTP_TRANSPORT, GsonFactory.getDefaultInstance(), credential)
                 .setApplicationName("Google Slides API")
+                .build();
+    }
+
+    public Drive createDriveService(HttpRequestInitializer requestInitializer) throws GeneralSecurityException, IOException {
+        return new Drive.Builder(new NetHttpTransport(),
+                GsonFactory.getDefaultInstance(),
+                requestInitializer)
+                .setApplicationName("Drive samples")
                 .build();
     }
 }
