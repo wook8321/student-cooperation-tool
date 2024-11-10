ef23927dd0e7292b467f004160bda1fa82204d77
pyunye <dpwns2318@naver.com>
2024-11-10 18:45:12 +0900

    [Backend] feat : PresentationApiController - 발표자료를 pdf나 ppt형태 다운로드 메서드 구현

--- src/main/java/com/stool/studentcooperationtools/domain/presentation/controller/PresentationApiController.java
+++ src/main/java/com/stool/studentcooperationtools/domain/presentation/controller/PresentationApiController.java
@@ -1,13 +1,21 @@
 package com.stool.studentcooperationtools.domain.presentation.controller;
 
+import com.google.auth.http.HttpCredentialsAdapter;
 import com.stool.studentcooperationtools.domain.api.ApiResponse;
 import com.stool.studentcooperationtools.domain.presentation.controller.response.PresentationFindResponse;
 import com.stool.studentcooperationtools.domain.presentation.service.PresentationService;
+import com.stool.studentcooperationtools.security.oauth2.dto.SessionMember;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,6 +27,30 @@ public class PresentationApiController {
     public ApiResponse<PresentationFindResponse> findPresentation(@PathVariable("roomId") Long roomId){
         PresentationFindResponse response = presentationService.findPresentation(roomId);
         return ApiResponse.of(HttpStatus.OK,response);
+    }
+
+    @GetMapping("/api/v1/presentation/{presentationId}/exportPdf")
+    public void exportToPdf(@PathVariable("presentationId") Long presentationId, HttpCredentialsAdapter credentialsAdapter,
+                                                  HttpServletResponse response) throws IOException, GeneralSecurityException {
+        ByteArrayOutputStream pdfStream = presentationService.exportPdf(credentialsAdapter, presentationId);
+        response.setContentType("application/pdf");
+        response.setHeader("Content-Disposition", "attachment; filename=\"exported.pdf\"");
+
+        // ByteArrayOutputStream의 데이터를 응답으로 전송
+        response.getOutputStream().write(pdfStream.toByteArray());
+        response.getOutputStream().flush();
+    }
+
+    @GetMapping("/api/v1/presentation/{presentationId}/exportPptx")
+    public void exportToPptx(Long presentationId, HttpCredentialsAdapter credentialsAdapter,
+                                                   HttpServletResponse response) throws IOException, GeneralSecurityException {
+        ByteArrayOutputStream pdfStream = presentationService.exportPpt(credentialsAdapter, presentationId);
+        response.setContentType("application/vnd.openxmlformats-officedocument.presentationml.presentation");
+        response.setHeader("Content-Disposition", "attachment; filename=\"exported.pptx\"");
+
+        // ByteArrayOutputStream의 데이터를 응답으로 전송
+        response.getOutputStream().write(pdfStream.toByteArray());
+        response.getOutputStream().flush();
     }
 
 }
