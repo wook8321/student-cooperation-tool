e72951572d909b287b09e8fc573733120238d435
pyunye <dpwns2318@naver.com>
2024-11-15 17:31:41 +0900

    [BackEnd] refactor : GoogleCredentialProvider - 쓸모 없는 변수 삭제

--- src/main/java/com/stool/studentcooperationtools/security/credential/GoogleCredentialProvider.java
+++ src/main/java/com/stool/studentcooperationtools/security/credential/GoogleCredentialProvider.java
@@ -2,15 +2,9 @@ package com.stool.studentcooperationtools.security.credential;
 
 import com.google.api.client.auth.oauth2.BearerToken;
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.http.HttpRequestInitializer;
-import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.slides.v1.SlidesScopes;
 import com.google.auth.http.HttpCredentialsAdapter;
@@ -30,21 +24,13 @@ public class GoogleCredentialProvider {
     private final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
     private final List<String> SCOPES = Collections.singletonList(SlidesScopes.PRESENTATIONS);
 
-    private final String credentialsFilePath;
-
-    private final String tokensDirectoryPath;
-
     private final String credentialsforupdateFilePath;
 
     private Credential credential;
 
     private HttpCredentialsAdapter credentialsAdapter;
 
-    public GoogleCredentialProvider(@Value("${google.slides.credentials-file-path}") String credentialsFilePath,
-                                    @Value("${google.slides.tokens-directory-path}") String tokensDirectoryPath,
-                                    @Value("${google.slides.credentials-forupdate-file-path}") String credentialsforupdateFilePath) throws IOException {
-        this.credentialsFilePath = credentialsFilePath;
-        this.tokensDirectoryPath = tokensDirectoryPath;
+    public GoogleCredentialProvider(@Value("${google.slides.credentials-forupdate-file-path}") String credentialsforupdateFilePath) throws IOException {
         this.credentialsforupdateFilePath = credentialsforupdateFilePath;
     }
 
