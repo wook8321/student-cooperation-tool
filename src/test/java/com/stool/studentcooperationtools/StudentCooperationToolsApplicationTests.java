3f604bf41476f692697e1318cdaed93c79fc2696
laipuni <2tgb02023@gmail.com>
2024-11-17 20:46:59 +0900

    [BackEnd] fix : StudentCooperationToolsApplicationTests에 @ActiveProfiles("test") 추가
    StudentCooperationToolsApplicationTests가 profile을 설정하지 않아서 배포환경

--- src/test/java/com/stool/studentcooperationtools/StudentCooperationToolsApplicationTests.java
+++ src/test/java/com/stool/studentcooperationtools/StudentCooperationToolsApplicationTests.java
@@ -2,8 +2,10 @@ package com.stool.studentcooperationtools;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
+@ActiveProfiles("test")
 @SpringBootTest
 class StudentCooperationToolsApplicationTests {
     @Test
