bfb536e24b457d9227b8957f9b6acceaa665f1b3
laipuni <2tgb02023@gmail.com>
2024-11-17 21:28:00 +0900

    [BackEnd] test : StudentCooperationToolsApplicationTests에 @TestPropertySource 추가

--- src/test/java/com/stool/studentcooperationtools/StudentCooperationToolsApplicationTests.java
+++ src/test/java/com/stool/studentcooperationtools/StudentCooperationToolsApplicationTests.java
@@ -3,10 +3,12 @@ package com.stool.studentcooperationtools;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 
 @ActiveProfiles("test")
-@SpringBootTest
+@TestPropertySource(properties = "spring.config.location=classpath:application.yml")
+@SpringBootTest(classes = StudentCooperationToolsApplication.class)
 class StudentCooperationToolsApplicationTests {
     @Test
     void contextLoads() {
