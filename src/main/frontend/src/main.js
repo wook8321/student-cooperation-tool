7316daeb4adcb45f6f46dfdc4f9c0e61d898b196
laipuni <2tgb02023@gmail.com>
2024-11-25 10:11:42 +0900

    [FrontEnd] design : main.js에 메인 화면 변경

--- src/main/frontend/src/main.js
+++ src/main/frontend/src/main.js
@@ -4,16 +4,19 @@ import Footer from "./footer";
 
 
 const Main = () => {
-  return (
-    <div className="container">
-      <main>
-        <h1 className="main_title">STOOL</h1>
-      </main>
-
-
-      <Footer/>
-    </div>
-  );
+    return (
+        <div className="container">
+            <main className="main-page">
+                <h1 className="main-title">STOOL에 오신 것을 환영합니다!</h1>
+                <p className="main-description">
+                    저희는 보다 간편한 협업툴을 지향합니다.<br />
+                    학생 분들을 보다 간편하고 즐거운 협업을 도와주기 위함이 저희 목적입니다.
+                </p>
+                <button className="gradient-button">Let's Go!</button>
+            </main>
+           <Footer/>
+        </div>
+    );
 };
 
 export default Main;
