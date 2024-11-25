c6a5a1c83d7c47360755e563d73a08971e12e22e
laipuni <2tgb02023@gmail.com>
2024-11-25 10:24:39 +0900

    [FrontEnd] design : juaFont.css 추가

--- src/main/frontend/src/main.js
+++ src/main/frontend/src/main.js
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
 import "./main.css";
 import Footer from "./footer";
+import "./juaFont.css"
 
 
 const Main = () => {
     return (
         <div className="container">
             <main className="main-page">
-                <h1 className="main-title">STOOL에 오신 것을 환영합니다!</h1>
-                <p className="main-description">
+                <h1 className="main-title jua-regular">STOOL에 오신 것을 환영합니다!</h1>
+                <p className="main-description jua-regular">
                     저희는 보다 간편한 협업툴을 지향합니다.<br />
                     학생 분들을 보다 간편하고 즐거운 협업을 도와주기 위함이 저희 목적입니다.
                 </p>
