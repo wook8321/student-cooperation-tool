566662fdb36873e26f5ae69558b787c7701a4a26
wook8321 <wook8321@knu.ac.kr>
2024-11-26 00:45:56 +0900

    [FrontEnd] fix : App.js 및 썸네일 미리보기 수정

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -6,6 +6,8 @@ import Friend from './friend.js';
 import Project from './project.js';
 import Topic from './topic.js';
 import Part from './part.js';
+import PPT from './ppt.js';
+import Slide from './slide.js';
 import Auth from "./auth";
 
 const App = () => {
@@ -19,8 +21,9 @@ const App = () => {
           <Route path = "/project" element = {<Auth><Project /></Auth>}></Route>
           <Route path = "/topic" element = {<Auth><Topic /></Auth>}></Route>
           <Route path = "/part" element = {<Auth><Part /></Auth>}></Route>
-          {/*일치하는 라우트가 없는경우 처리 
-					<Route path="/*" element={<NotFound />}></Route> */}
+          <Route path = "/ppt" element = {<Auth><PPT /></Auth>}></Route>
+          <Route path = "/slide" element = {<Auth><Slide /></Auth>}></Route>
+          {/*일치하는 라우트가 없는경우 처리 <Route path="/*" element={<NotFound />}></Route> */}
         </Routes>
       </BrowserRouter>
     </div>
