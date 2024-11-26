9528ad31efecf67831d4e245678d19933302065c
laipuni <2tgb02023@gmail.com>
2024-11-26 22:29:46 +0900

    [FrontEnd] feat : App.js에 <Route> topic 추가

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -5,6 +5,7 @@ import Main from './main.js';
 import Friend from './friend.js';
 import Project from './project.js';
 import Auth from "./auth";
+import Topic from "./topic";
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
           <Route path = "/login" element = {<Login />}></Route>
           <Route path = "/friend" element = {<Auth><Friend /></Auth>}></Route>
           <Route path = "/project" element = {<Auth><Project /></Auth>}></Route>
+          <Route path = "/topic" element = {<Auth><Topic /></Auth>}></Route>
           {/*일치하는 라우트가 없는경우 처리 <Route path="/*" element={<NotFound />}></Route> */}
         </Routes>
       </BrowserRouter>
