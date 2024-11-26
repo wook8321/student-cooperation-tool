52c535b2afd04ebc6dd83709ff509f78c90756aa
laipuni <2tgb02023@gmail.com>
2024-11-26 23:07:41 +0900

    [BackEnd] feat : VoteAddWebSocketResponse에 topicId, voteNum 추가
    투표를 추가한 주제의 id와 투표수를 나타내는 값들이다.

--- src/main/java/com/stool/studentcooperationtools/websocket/controller/vote/response/VoteAddWebSocketResponse.java
+++ src/main/java/com/stool/studentcooperationtools/websocket/controller/vote/response/VoteAddWebSocketResponse.java
@@ -1,6 +1,7 @@
 package com.stool.studentcooperationtools.websocket.controller.vote.response;
 
 import com.stool.studentcooperationtools.domain.member.Member;
+import com.stool.studentcooperationtools.domain.topic.Topic;
 import com.stool.studentcooperationtools.domain.vote.Vote;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,17 +10,23 @@ import lombok.Getter;
 public class VoteAddWebSocketResponse {
 
     private Long voteId;
+    private Long topicId;
+    private int voteNum;
     private Long memberId;
 
     @Builder
-    private VoteAddWebSocketResponse(final Long voteId, final Long memberId) {
+    private VoteAddWebSocketResponse(final Long voteId, final int voteNum, final Long topicId, final Long memberId) {
         this.voteId = voteId;
+        this.topicId = topicId;
+        this.voteNum = voteNum;
         this.memberId = memberId;
     }
 
-    public static VoteAddWebSocketResponse of(Vote vote, Member member){
+    public static VoteAddWebSocketResponse of(final Vote vote, final Member member,final Topic topic){
         return VoteAddWebSocketResponse.builder()
                 .voteId(vote.getId())
+                .topicId(topic.getId())
+                .voteNum(topic.getVotes().size())
                 .memberId(member.getId())
                 .build();
     }
