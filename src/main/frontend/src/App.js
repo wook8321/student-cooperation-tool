8d77dfddc186dc48917db75927db168ee4226f07
laipuni <2tgb02023@gmail.com>
2024-11-27 19:16:12 +0900

    [FrontEnd] feat : App.js에 WebsocketContext.js 임포트 문 추가 및 웹소켓 부분 수정

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { WebSocketProvider } from './WebsocketContext';
 import Login from './login.js';
 import Main from './main.js';
 import Friend from './friend.js';
 import Project from './project.js';
 import Auth from "./auth";
 import Topic from "./topic";
+import Part from "./part";
 
 const App = () => {
   return (
@@ -16,12 +18,12 @@ const App = () => {
           <Route path = "/login" element = {<Login />}></Route>
           <Route path = "/friend" element = {<Auth><Friend /></Auth>}></Route>
           <Route path = "/project" element = {<Auth><Project /></Auth>}></Route>
-          <Route path = "/topic" element = {<Auth><Topic /></Auth>}></Route>
+          <Route path = "/topic" element = {<WebSocketProvider><Topic /></WebSocketProvider>}></Route>
+          <Route path = "/part" element = {<WebSocketProvider><Part /></WebSocketProvider>}></Route>
           {/*일치하는 라우트가 없는경우 처리 <Route path="/*" element={<NotFound />}></Route> */}
         </Routes>
       </BrowserRouter>
     </div>
-
   );
 };
 
