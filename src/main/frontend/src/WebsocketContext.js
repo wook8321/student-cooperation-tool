1f78ffa17f08ca02e9def4b8582a0364b478a52c
laipuni <2tgb02023@gmail.com>
2024-11-27 19:17:34 +0900

    [FrontEnd] feat : WebsocketContext.js 생성
    WebsocketContext는 웹소켓을 사용할 때 연결을 보조해주는 context 역할로 생성됨, 각각의 웹소켓 연결을 하면 끊어지기 때문에 계속 웹소켓 연결을 유지할 수 있다.

---
+++ src/main/frontend/src/WebsocketContext.js
@@ -0,0 +1,51 @@
+import React, { createContext, useContext, useRef, useState, useEffect } from 'react';
+import {useLocation} from 'react-router-dom';// 방 id를 받아오기 위해 선언한 import
+import { Client } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+import { domain } from './domain';
+
+const WebSocketContext = createContext();
+
+export const useWebSocket = () => {
+    return useContext(WebSocketContext);
+};
+
+export const WebSocketProvider = ({ children }) => {
+    const [isConnected, setIsConnected] = useState(false);
+    const stompClient = useRef(null);
+    const location = useLocation();
+    const { roomId, subUrl } = location.state || {};
+    const onStompError = (error) => {
+        //2-2 연결 실패의 경우
+        alert("방에 입장에 실패하였습니다.");
+        console.error("STOMP Error", error);
+        window.location.href = "/";
+    }
+
+    useEffect(() => {
+        alert("소켓 연결중, roomId = " + roomId + " subUrl = " + subUrl)
+        stompClient.current = new Client({
+            webSocketFactory: () => new SockJS(`${domain}/ws-stomp`),
+            connectHeaders: {
+                SubscribeUrl : subUrl // 어디에 구독할 지 헤더에 담아서 보냄
+            },
+            reconnectDelay: 5000,
+            onConnect: () => setIsConnected(true),
+            onStompError,
+        });
+
+        stompClient.current.activate();
+
+        return () => {
+            if (stompClient.current) {
+                stompClient.current.deactivate();
+            }
+        };
+    }, []);
+
+    return (
+        <WebSocketContext.Provider value={{ stompClient, isConnected, roomId}}>
+            {children}
+        </WebSocketContext.Provider>
+    );
+};
