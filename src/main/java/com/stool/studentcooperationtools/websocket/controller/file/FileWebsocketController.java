f65eec1a3599e55b490560bc5a41fcb994cffd06
laipuni <2tgb02023@gmail.com>
2024-11-30 16:26:46 +0900

    [BackEnd] feat : FileWebsocketController에 fileUpload 파라미터 변경
    웹소켓 방식으로 파일 업로드는 연결 취소가 될 가능성이 있어 http로 업로드한 파일의 정보를 웹소켓으로 통신하는 방식으로 변경

--- src/main/java/com/stool/studentcooperationtools/websocket/controller/file/FileWebsocketController.java
+++ src/main/java/com/stool/studentcooperationtools/websocket/controller/file/FileWebsocketController.java
@@ -4,8 +4,8 @@ import com.stool.studentcooperationtools.domain.file.service.FileService;
 import com.stool.studentcooperationtools.s3.S3Service;
 import com.stool.studentcooperationtools.security.oauth2.dto.SessionMember;
 import com.stool.studentcooperationtools.websocket.controller.Utils.SimpleMessageSendingUtils;
-import com.stool.studentcooperationtools.websocket.controller.file.response.FileDeleteWebsocketResponse;
 import com.stool.studentcooperationtools.websocket.controller.file.request.FileDeleteWebsocketRequest;
+import com.stool.studentcooperationtools.websocket.controller.file.response.FileDeleteWebsocketResponse;
 import com.stool.studentcooperationtools.websocket.controller.file.response.FileUploadWebsocketResponse;
 import com.stool.studentcooperationtools.websocket.controller.request.FileUploadWebsocketRequest;
 import com.stool.studentcooperationtools.websocket.controller.request.WebsocketResponse;
@@ -15,9 +15,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
-
-import java.util.HashMap;
-import java.util.List;
 
 import static com.stool.studentcooperationtools.websocket.WebsocketMessageType.PART_FILE_REMOVE;
 import static com.stool.studentcooperationtools.websocket.WebsocketMessageType.PART_FILE_UPLOAD;
@@ -32,12 +29,10 @@ public class FileWebsocketController {
     private final FileService fileService;
 
     @MessageMapping("/file/upload")
-    public void fileUpload(@Valid @RequestBody FileUploadWebsocketRequest request, SessionMember sessionMember){
-        HashMap<String, List<String>> fileSet = s3Service.uploadFile(request.getFileName(), request.getFileCode());
-        FileUploadWebsocketResponse response = fileService.addFile(request,fileSet,sessionMember);
+    public void fileUpload(@Valid @RequestBody FileUploadWebsocketRequest request){
         sendingUtils.convertAndSend(
                 sendingUtils.creatPartResearchSubUrl(request.getRoomId()),
-                WebsocketResponse.of(PART_FILE_UPLOAD,response)
+                WebsocketResponse.of(PART_FILE_UPLOAD, FileUploadWebsocketResponse.of(request))
         );
     }
 
