5ba95b08bc8b3d16715e95d51ca8f0468bddc669
laipuni <2tgb02023@gmail.com>
2024-11-30 16:23:58 +0900

    [BackEnd] feat : FileUploadDto에 fileType,fileUrl 추가
    (fileType : 파일의 유형, fileUrl : s3에 저장된 파일의 경로) 미리보기를 위해 필드 변수 추가

--- src/main/java/com/stool/studentcooperationtools/websocket/controller/file/response/FileUploadDto.java
+++ src/main/java/com/stool/studentcooperationtools/websocket/controller/file/response/FileUploadDto.java
@@ -1,6 +1,8 @@
 package com.stool.studentcooperationtools.websocket.controller.file.response;
 
 import com.stool.studentcooperationtools.domain.file.File;
+import com.stool.studentcooperationtools.domain.file.FileType;
+import com.stool.studentcooperationtools.s3.S3Service;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,22 +13,29 @@ import lombok.NoArgsConstructor;
 public class FileUploadDto {
 
     private Long fileId;
-    private String originalFileName;
+    private String originalName;
+    private FileType fileType;
     private String fileName;
+    private String fileUrl;
 
     @Builder
-    private FileUploadDto(final Long fileId, final String originalFileName, final String fileName) {
+    private FileUploadDto(final Long fileId, final String originalName,final FileType fileType, final String fileName,final String fileUrl) {
         this.fileId = fileId;
-        this.originalFileName = originalFileName;
+        this.originalName = originalName;
+        this.fileType = fileType;
         this.fileName = fileName;
+        this.fileUrl = fileUrl;
     }
 
     public static FileUploadDto of(final File file){
         return FileUploadDto.builder()
                 .fileId(file.getId())
                 .fileName(file.getFileName())
-                .originalFileName(file.getOriginalName())
+                .fileType(file.getFileType())
+                .originalName(file.getOriginalName())
+                .fileUrl(S3Service.getS3FileUrl(file.getFileName()))
                 .build();
     }
+
 
 }
