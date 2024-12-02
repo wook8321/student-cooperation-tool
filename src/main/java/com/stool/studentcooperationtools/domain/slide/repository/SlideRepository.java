c0f4b7f69f677a4c534220ce1ffa5ca5cab1ace0
pyunye <dpwns2318@naver.com>
2024-12-02 19:39:40 +0900

    [Backend] feat : SlideRepository - ppt의 슬라이드와 스크립트 삭제 기능 및 테스트 구현

--- src/main/java/com/stool/studentcooperationtools/domain/slide/repository/SlideRepository.java
+++ src/main/java/com/stool/studentcooperationtools/domain/slide/repository/SlideRepository.java
@@ -2,9 +2,11 @@ package com.stool.studentcooperationtools.domain.slide.repository;
 
 import com.stool.studentcooperationtools.domain.slide.Slide;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,5 +15,11 @@ public interface SlideRepository extends JpaRepository<Slide, Long> {
 
     @Query(value = "select s from Slide s join fetch s.script where s.presentation.id = :presentationId")
     List<Slide> findSlidesAndScriptsByPresentationId(@Param("presentationId") Long presentationId);
+
+    @Modifying
+    @Transactional
+    @Query(value = "delete from Slide s where s.presentation.id = :presentationId")
+    void deleteByPresentationId(@Param("presentationId") Long presentationId);
+
 
 }
