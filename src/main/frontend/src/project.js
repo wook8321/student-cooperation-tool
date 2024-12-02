672b6fa9dc9cea65f1345e69c3d23c304688235d
laipuni <2tgb02023@gmail.com>
2024-12-02 20:18:09 +0900

    [FrontEnd] fix : project.js 방입장 이슈 해결
    비밀 번호 창이 열리지 않은 이유는 잘못된 문법으로 인해 열리지 않았다.

--- src/main/frontend/src/project.js
+++ src/main/frontend/src/project.js
@@ -549,10 +549,10 @@ const Project = () => {
                                                                     <div
                                                                         className={`image-cap ${randomCapClass}`}>{room.title}</div>
                                                                     <div className="card-body">
-                                                                        <h3 className="card-title">{room.topic}</h3>
                                                                         <span style={{fontSize: "small", justifyContent: "end", color: "white"}}>
                                                                             참가자 : {room.participationNum}
                                                                         </span>
+                                                                        <h3 className="card-title">{room.topic}</h3>
                                                                         <button className="card-button"
                                                                                 onClick={() => enterRoom(room.roomId, room.title)}>
                                                                             참여하기
@@ -630,11 +630,10 @@ const Project = () => {
                             <button className="close_button" onClick={() => closeEnterModal()}>
                       X
                   </button>
-                  <div id="passwordInvalidDiv"></div>
                   <label className="enter_modal_label">
                       제목 : {enterRoomTitle}
                   </label>
-                  <div id="passwordInvalidDiv" style="color: #1a1d20">비밀번호를 입력해주세요.</div>
+                  <div id="passwordInvalidDiv" style={{color : "gray"}}>비밀번호를 입력해주세요.</div>
                   <input className="enter_modal_input" id="roomPasswordInput" type="password"/>
                   <button className="enter_button" onClick={() => verifyPasswordAndEnterRoom(enterRoomId)}> 입장 </button>
               </div>
