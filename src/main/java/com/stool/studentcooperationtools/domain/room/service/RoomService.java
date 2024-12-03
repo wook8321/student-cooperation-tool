5080995cee6d72845f019d9b2f4165f3e6bcb7f4
pyunye <dpwns2318@naver.com>
2024-12-03 18:33:40 +0900

    [Backend] refactor : RoomService 방 삭제 메서드 변경

--- src/main/java/com/stool/studentcooperationtools/domain/room/service/RoomService.java
+++ src/main/java/com/stool/studentcooperationtools/domain/room/service/RoomService.java
@@ -7,6 +7,7 @@ import com.stool.studentcooperationtools.domain.member.repository.MemberReposito
 import com.stool.studentcooperationtools.domain.part.repository.PartRepository;
 import com.stool.studentcooperationtools.domain.participation.Participation;
 import com.stool.studentcooperationtools.domain.participation.repository.ParticipationRepository;
+import com.stool.studentcooperationtools.domain.presentation.Presentation;
 import com.stool.studentcooperationtools.domain.presentation.repository.PresentationRepository;
 import com.stool.studentcooperationtools.domain.presentation.service.PresentationService;
 import com.stool.studentcooperationtools.domain.room.Room;
@@ -19,6 +20,8 @@ import com.stool.studentcooperationtools.domain.room.controller.response.RoomEnt
 import com.stool.studentcooperationtools.domain.room.controller.response.RoomSearchResponse;
 import com.stool.studentcooperationtools.domain.room.controller.response.RoomsFindResponse;
 import com.stool.studentcooperationtools.domain.room.repository.RoomRepository;
+import com.stool.studentcooperationtools.domain.script.repository.ScriptRepository;
+import com.stool.studentcooperationtools.domain.slide.repository.SlideRepository;
 import com.stool.studentcooperationtools.domain.topic.repository.TopicRepository;
 import com.stool.studentcooperationtools.security.oauth2.dto.SessionMember;
 import lombok.RequiredArgsConstructor;
@@ -46,6 +49,8 @@ public class RoomService {
     private final ChatRepository chatRepository;
     private final PartRepository partRepository;
     private final PresentationRepository presentationRepository;
+    private final SlideRepository slideRepository;
+    private final ScriptRepository scriptRepository;
 
     public RoomsFindResponse findRooms(SessionMember member, final int page) {
         Pageable pageable = PageRequest.of(page, PagingUtils.ROOM_PAGING_PARSE);
@@ -91,19 +96,18 @@ public class RoomService {
         Room room = roomRepository.findRoomWithPLock(request.getRoomId())
                 .orElseThrow(() -> new IllegalArgumentException("방 id 오류"));
         if(Objects.equals(member.getMemberSeq(), room.getLeader().getId())){
-            try {
-                presentationService.deletePresentation(request.getRoomId());
-            } catch(IllegalStateException e){
-                throw new IllegalStateException(e.getMessage(), e.getCause());
-            }
-            finally {
                 chatRepository.deleteByRoomId(room.getId());
                 partRepository.deleteByRoomId(room.getId());
                 topicRepository.deleteByRoomId(room.getId());
+                if(presentationRepository.existsByRoomId(room.getId())){
+                    Presentation presentation = presentationRepository.findByRoomId(room.getId())
+                                    .orElseThrow(()->new IllegalArgumentException("방의 ppt가 없습니다"));
+                    slideRepository.deleteByPresentationId(presentation.getId());
+                    scriptRepository.deleteByPresentationId(presentation.getId());
+                }
                 presentationRepository.deleteByRoomId(room.getId());
                 participationRepository.deleteByRoomId(room.getId());
                 roomRepository.deleteById(room.getId());
-            }
         }
         else{
             Member teammate = memberRepository.findById(member.getMemberSeq())
