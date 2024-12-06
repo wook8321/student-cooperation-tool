af18087bbfdd74140ef0e3a3dfe57a38446ee991
pyunye <dpwns2318@naver.com>
2024-12-06 16:35:09 +0900

    [Backend] feat : FriendshipRepository - 친구관계 삭제 메서드 구현

--- src/main/java/com/stool/studentcooperationtools/domain/friendship/repository/FriendshipRepository.java
+++ src/main/java/com/stool/studentcooperationtools/domain/friendship/repository/FriendshipRepository.java
@@ -2,9 +2,14 @@ package com.stool.studentcooperationtools.domain.friendship.repository;
 
 import com.stool.studentcooperationtools.domain.friendship.Friendship;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
-
+    @Modifying
+    @Query(value = "delete from Friendship f where f.me.id = :memberId and f.friend.id = :friendId")
+    public void deleteByFriendId(@Param("memberId") Long memberId, @Param("friendId")Long friendId);
 }
